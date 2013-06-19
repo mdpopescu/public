1d295aee6007d6223bebb38dab94a06d36bd86d0
Marcel Popescu <marcelp@clevertech.biz>
2013-06-19 17:57:19 +0300

    more unit tests for the TableComparer class

--- CompareExcelFiles2/CompareExcelFiles2.Library/TableComparer.cs
+++ CompareExcelFiles2/CompareExcelFiles2.Library/TableComparer.cs
@@ -1,15 +1,54 @@
-﻿namespace Renfield.CompareExcelFiles2.Library
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Renfield.CompareExcelFiles2.Library
 {
   public class TableComparer
   {
     public TableComparer(string[] columns)
     {
-      //
+      this.columns = columns;
     }
 
     public Table Compare(Table table1, Table table2)
     {
-      return table1;
+      var indices = GetIndices(table1.Columns).ToList();
+
+      var rows = table1
+        .Data
+        .Where(row => !RowFoundIn(row, table2.Data, indices));
+
+      return new MemoryTable(Enumerable.Repeat(table1.Columns, 1).Concat(rows));
+    }
+
+    //
+
+    private readonly string[] columns;
+
+    private IEnumerable<int> GetIndices(string[] tableColumns)
+    {
+      return columns.Select(col => Array.IndexOf(tableColumns, col));
+    }
+
+    private static bool RowFoundIn(IList<string> row, IEnumerable<string[]> data, IEnumerable<int> indices)
+    {
+      // can't use .FirstOrDefault() here, the default is 0
+      var comparisons = data
+        .Select(otherRow => CompareRows(row, otherRow, indices))
+        .Where(comparison => comparison == 0)
+        .Take(1)
+        .ToList();
+
+      return comparisons.Any();
+    }
+
+    private static int CompareRows(IList<string> row1, IList<string> row2, IEnumerable<int> indices)
+    {
+      return indices
+        .Select(index => string.Compare(row1[index], row2[index], StringComparison.InvariantCultureIgnoreCase))
+        .Where(comparison => comparison != 0)
+        .FirstOrDefault();
     }
   }
 }
--- CompareExcelFiles2/Tests/TableComparerTests.cs
+++ CompareExcelFiles2/Tests/TableComparerTests.cs
@@ -46,6 +46,35 @@ namespace Renfield.Tests
         CollectionAssert.AreEqual(new[] { "3", "4" }, result.Data[0]);
         CollectionAssert.AreEqual(new[] { "7", "8" }, result.Data[1]);
       }
+
+      [TestMethod]
+      public void ReturnsRowsThatDifferInMultipleColumns()
+      {
+        var table1 = new MemoryTable(new[]
+        {
+          new[] { "A", "B" },
+          new[] { "1", "0" },
+          new[] { "1", "2" },
+          new[] { "1", "4" },
+          new[] { "1", "6" },
+          new[] { "1", "8" },
+        });
+        var table2 = new MemoryTable(new[]
+        {
+          new[] { "A", "B" },
+          new[] { "1", "2" },
+          new[] { "2", "0" },
+          new[] { "1", "0" },
+        });
+        var sut = new TableComparer(new[] { "A", "B" });
+
+        var result = sut.Compare(table1, table2);
+
+        Assert.AreEqual(3, result.RowCount);
+        CollectionAssert.AreEqual(new[] { "1", "4" }, result.Data[0]);
+        CollectionAssert.AreEqual(new[] { "1", "6" }, result.Data[1]);
+        CollectionAssert.AreEqual(new[] { "1", "8" }, result.Data[2]);
+      }
     }
   }
 }
