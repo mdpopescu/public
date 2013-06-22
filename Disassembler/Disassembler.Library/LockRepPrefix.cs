454a89e2ef08d90ec24169f7e9a55d9223ff587c
Marcel Popescu <marcelp@clevertech.biz>
2013-06-22 05:40:15 +0300

    abandoning this

---
+++ Disassembler/Disassembler.Library/LockRepPrefix.cs
@@ -0,0 +1,9 @@
+﻿namespace Renfield.Disassembler.Library
+{
+  public enum LockRepPrefix
+  {
+    LOCK = 0xF0,
+    REPNZ = 0xF2,
+    REPZ = 0xF3,
+  }
+}
---
+++ Disassembler/Disassembler.Library/SegPrefix.cs
@@ -0,0 +1,12 @@
+﻿namespace Renfield.Disassembler.Library
+{
+  public enum SegPrefix
+  {
+    SegCS = 0x2E,
+    SegSS = 0x36,
+    SegDS = 0x3E,
+    SegES = 0x26,
+    SegFS = 0x64,
+    SegGS = 0x65,
+  }
+}
