b1c9ca736fe3474c89bd14fd399a5f5ea5e8e6dc
Marcel Popescu <marcelp@clevertech.biz>
2013-06-22 13:01:36 +0300

    initial commit for the Inventory project

---
+++ Inventory/Inventory.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Renfield.Inventory.Controllers;
+
+namespace Renfield.Inventory.Tests.Controllers
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+    [TestMethod]
+    public void Index()
+    {
+      // Arrange
+      var controller = new HomeController();
+
+      // Act
+      var result = controller.Index() as ViewResult;
+
+      // Assert
+      Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
+    }
+
+    [TestMethod]
+    public void About()
+    {
+      // Arrange
+      var controller = new HomeController();
+
+      // Act
+      var result = controller.About() as ViewResult;
+
+      // Assert
+      Assert.IsNotNull(result);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Public repository - CC-by-SA 3.0
 * CompareExcelFiles2: the same project as above but started from scratch using TDD; article at http://mdpopescu.blogspot.com/2013/06/comparing-excel-files-take-two.html
 * Failover: fail-over algorithm; see article at http://mdpopescu.blogspot.com/2013/06/fail-over-algorithm.html
 * Hot Folder Windows Service: Windows service to monitor a specific folder and (for now) log the create / delete / change / rename events
+* Inventory: inventory management system - mainly to use SignalR and Knockout.js
 * RecursiveCompare: recursive comparison of two objects' properties (if some of those properties are objects, compare those using the same algorithm); incomplete
