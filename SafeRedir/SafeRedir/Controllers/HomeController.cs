00e224d82d0ca9dbfe6a3d2f5a05513ca1f895d0
Marcel Popescu <marcelp@clevertech.biz>
2013-06-23 17:37:44 +0300

    initial commit of SafeRedir

---
+++ SafeRedir/SafeRedir/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Renfield.SafeRedir.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      ViewBag.Message = "Safe (time-limited) URL redirector";
+
+      return View();
+    }
+  }
+}
