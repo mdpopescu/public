d379d17fcec30fc400462cd5f2bde76374e9b6dd
Marcel Popescu <marcelp@clevertech.biz>
2013-06-23 20:21:04 +0300

    Added UrlInfo class and tests

---
+++ SafeRedir/SafeRedir.Tests/Data/UrlInfoTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Renfield.SafeRedir.Data;
+
+namespace Renfield.SafeRedir.Tests.Data
+{
+  [TestClass]
+  public class UrlInfoTests
+  {
+    [TestClass]
+    public class GetUrl : UrlInfoTests
+    {
+      [TestMethod]
+      public void ReturnsSafeUrlAfterExpirationDate()
+      {
+        var sut = new UrlInfo { OriginalUrl = "a", SafeUrl = "b", ExpiresAt = new DateTime(2000, 1, 2, 3, 4, 5) };
+
+        var result = sut.GetUrl(new DateTime(2000, 1, 2, 3, 4, 6));
+
+        Assert.AreEqual("b", result);
+      }
+
+      [TestMethod]
+      public void ReturnsSafeUrlExactlyOnExpirationDate()
+      {
+        var sut = new UrlInfo { OriginalUrl = "a", SafeUrl = "b", ExpiresAt = new DateTime(2000, 1, 2, 3, 4, 5) };
+
+        var result = sut.GetUrl(new DateTime(2000, 1, 2, 3, 4, 5));
+
+        Assert.AreEqual("b", result);
+      }
+
+      [TestMethod]
+      public void ReturnsOriginalUrlBeforeExpirationDate()
+      {
+        var sut = new UrlInfo { OriginalUrl = "a", SafeUrl = "b", ExpiresAt = new DateTime(2000, 1, 2, 3, 4, 5) };
+
+        var result = sut.GetUrl(new DateTime(2000, 1, 2, 3, 4, 4));
+
+        Assert.AreEqual("a", result);
+      }
+    }
+  }
+}
---
+++ SafeRedir/SafeRedir/Data/UrlInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Renfield.SafeRedir.Data
+{
+  public class UrlInfo
+  {
+    public string Id { get; set; }
+    public string OriginalUrl { get; set; }
+    public string SafeUrl { get; set; }
+    public DateTime ExpiresAt { get; set; }
+
+    public string GetUrl(DateTime when)
+    {
+      return when >= ExpiresAt ? SafeUrl : OriginalUrl;
+    }
+  }
+}
