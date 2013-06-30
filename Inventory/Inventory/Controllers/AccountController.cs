1406da0ad45107de070aa532c5c1e9901a4b0d00
Marcel Popescu <marcelp@clevertech.biz>
2013-06-30 10:17:45 +0300

    giving up, going back to VS2010 / MVC 3

--- Inventory/Inventory/Controllers/AccountController.cs
+++ Inventory/Inventory/Controllers/AccountController.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
-using Microsoft.Owin.Security;
 using Renfield.Inventory.Models;
 
 namespace Renfield.Inventory.Controllers
--- Inventory/Inventory/Startup.cs
+++ Inventory/Inventory/Startup.cs
@@ -1,12 +1,6 @@
-﻿using Owin;
-
-namespace Renfield.Inventory
+﻿namespace Renfield.Inventory
 {
-    public partial class Startup 
-    {
-        public void Configuration(IAppBuilder app) 
-        {
-            ConfigureAuth(app);
-        }
-    }
+  public partial class Startup
+  {
+  }
 }
