a912a9daeb764287955ec78f8bf378f8e620985f
Marcel Popescu <marcelp@clevertech.biz>
2013-07-01 11:44:48 +0300

    almost there - the child table is updated but the main table disappears (in Acquisitions)

--- Inventory/Inventory/Scripts/helpers.js
+++ Inventory/Inventory/Scripts/helpers.js
@@ -7,17 +7,6 @@ JTable.Setup = function() {
   var table = $('.jtable');
   $('caption', table).addClass('ui-state-default');
   $('th', table).addClass('ui-state-default');
-  $('td', table).addClass('ui-widget-content');
-  $(table).delegate('tr', 'hover', function () {
-    $('td', $(this)).toggleClass('ui-state-hover');
-  });
-  $(table).delegate('tr', 'click', function() {
-    $('td', $(this)).toggleClass('ui-state-highlight');
-  });
-};
-
-JTable.SetupNewRows = function(rows) {
-  $(rows).find('td').addClass('ui-widget-content');
 };
 
 // date conversion
@@ -27,6 +16,7 @@ function parseJsonDate(jsonDateString) {
 }
 
 // assumes the presence of jQueryUI
+
 function formatDate(date) {
   return $.datepicker.formatDate('mm/dd/yy', date);
 }
