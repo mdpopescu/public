dfadff00017601214c695624badf903aa6e63df9
Marcel Popescu <marcelp@clevertech.biz>
2013-07-06 16:23:24 +0300

    generated one image

---
+++ QRMaker/QRMaker/Generator.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+
+namespace Renfield.QRMaker
+{
+  public class Generator
+  {
+    public Generator(QrEncoder encoder)
+    {
+      this.encoder = encoder;
+    }
+
+    public void Render(string data, Stream stream)
+    {
+      var qrCode = encoder.Encode(data);
+      var renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+      renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, stream);
+    }
+
+    //
+
+    private readonly QrEncoder encoder;
+  }
+}
--- QRMaker/QRMaker/Program.cs
+++ QRMaker/QRMaker/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Renfield.QRMaker
+﻿using System.IO;
+using Gma.QrCodeNet.Encoding;
+
+namespace Renfield.QRMaker
 {
-  class Program
+  internal class Program
   {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+      const string content = "http://mdpopescu.blogspot.com";
+
+      var generator = new Generator(new QrEncoder());
+
+      using (var ms = new MemoryStream())
+      {
+        generator.Render(content, ms);
+        ms.Seek(0, SeekOrigin.Begin);
+
+        using (var file = new FileStream("test.png", FileMode.Create, FileAccess.Write, FileShare.None))
+        {
+          ms.CopyTo(file);
+          file.Flush();
+        }
+      }
     }
   }
 }
