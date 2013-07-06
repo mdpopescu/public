2cf1f7efbf501da01db9e5ef64a38341d1c913d9
Marcel Popescu <marcelp@clevertech.biz>
2013-07-06 16:36:23 +0300

    creates zip with the images

--- QRMaker/QRMaker/Program.cs
+++ QRMaker/QRMaker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Gma.QrCodeNet.Encoding;
+using Ionic.Zip;
 
 namespace Renfield.QRMaker
 {
@@ -7,9 +8,13 @@ namespace Renfield.QRMaker
     private static void Main(string[] args)
     {
       var generator = new Generator(new QrEncoder());
-
       for (var i = 1; i <= 3; i++)
         generator.Render("Some data " + i, i + ".png");
+
+      var zipper = new ZipFile("123.zip");
+      for (var i = 1; i <= 3; i++)
+        zipper.AddFile(i + ".png");
+      zipper.Save();
     }
   }
 }
