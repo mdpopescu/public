95bed2bb20495b58a82ff1a7f585d0f885a4cb3c
mdpopescu <mdpopescu@gmail.com>
2013-08-10 09:55:48 +0100

    added a shuffling implementation

---
+++ VideoSpinner/VideoSpinner/RandomShuffler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Renfield.VideoSpinner
+{
+    public class RandomShuffler : Shuffler
+    {
+        public RandomShuffler(Random rnd)
+        {
+            this.rnd = rnd;
+        }
+
+        public IList<T> Shuffle<T>(IList<T> list)
+        {
+            // generate a list of N random numbers
+            // add the last part of the item hash to avoid collisions
+            var order = Enumerable
+                .Range(1, list.Count)
+                .Select(_ => rnd.Next(list.Count))
+                .Select((i, r) => r << 8 + list[i].GetHashCode() & 0xFF)
+                .ToList();
+
+            // return the original list ordered by the random ranks
+            return list
+                .Zip(order, (it, rank) => new {it, rank})
+                .OrderBy(pair => pair.rank)
+                .Select(pair => pair.it)
+                .ToList();
+        }
+
+        //
+
+        private readonly Random rnd;
+    }
+}
---
+++ VideoSpinner/VideoSpinner/Shuffler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Renfield.VideoSpinner
+{
+    public interface Shuffler
+    {
+        IList<T> Shuffle<T>(IList<T> list);
+    }
+}
