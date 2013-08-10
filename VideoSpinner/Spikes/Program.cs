0218653d40bc1105c1d16e29dca006fa9e619c31
mdpopescu <mdpopescu@gmail.com>
2013-08-10 10:10:09 +0100

    added a test program and fixed a bug in the spinner algorithm

---
+++ VideoSpinner/Spikes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Renfield.VideoSpinner;
+
+namespace Renfield.Spikes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var shuffler = new RandomShuffler(new Random());
+
+            var list = Enumerable.Range(1, 20).ToList();
+            for (var i = 1; i <= 5; i++)
+            {
+                var shuffled = shuffler.Shuffle(list);
+                Console.WriteLine(string.Join(",", shuffled.Select(it => it.ToString())));
+            }
+        }
+    }
+}
--- VideoSpinner/VideoSpinner/RandomShuffler.cs
+++ VideoSpinner/VideoSpinner/RandomShuffler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 namespace Renfield.VideoSpinner
@@ -13,12 +14,17 @@ namespace Renfield.VideoSpinner
 
         public IList<T> Shuffle<T>(IList<T> list)
         {
+            Contract.Requires(list.Any());
+            Contract.Requires(list.Count < 65536);
+
             // generate a list of N random numbers
             // add the last part of the item hash to avoid collisions
+            var N = list.Count;
+            var N2 = N * N;
             var order = Enumerable
-                .Range(1, list.Count)
-                .Select(_ => rnd.Next(list.Count))
-                .Select((i, r) => r << 8 + list[i].GetHashCode() & 0xFF)
+                .Range(1, N)
+                .Select(_ => rnd.Next(N2))
+                .Select((r, i) => (r << 8) | (list[i].GetHashCode() & 0xFF))
                 .ToList();
 
             // return the original list ordered by the random ranks
