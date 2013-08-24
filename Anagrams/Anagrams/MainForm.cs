3a379161a36b6579862b4ce6fcc97d75ae32ed19
mdpopescu <mdpopescu@gmail.com>
2013-08-24 22:46:59 +0100

    Initial commit

---
+++ Anagrams/Anagrams/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Renfield.Anagrams
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+  }
+}
