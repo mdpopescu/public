850352860dffd3fc621a9b11fbb4825222779dea
mdpopescu <mdpopescu@gmail.com>
2013-08-27 21:23:17 +0100

    simplified the fsm syntax

--- FSM/FSM.Library/FSM.cs
+++ FSM/FSM.Library/FSM.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace Renfield.FSM.Library
 {
   public class FSM<T>
   {
     public bool IgnoreErrors { get; set; }
+    public Action OnStart;
 
-    public FSM(Node<T> start)
+    public FSM(string initialState)
     {
-      current = start;
+      this.initialState = initialState;
+
+      nodes = new List<Node<T>>();
+      Restart();
+    }
+
+    public void Restart()
+    {
+      current = GetNode(initialState);
+      if (OnStart != null)
+        OnStart();
     }
 
     public void Handle(T input)
@@ -20,8 +34,34 @@ namespace Renfield.FSM.Library
         throw new Exception(string.Format("Unknown input [{0}]", input));
     }
 
+    public void Add(string state, Predicate<T> predicate, Action<T> action, string newState = null)
+    {
+      var node = GetNode(state);
+      node.Add(predicate, action, GetNode(newState ?? state));
+    }
+
+    public void Add(string state, T value, Action<T> action, string newState = null)
+    {
+      var node = GetNode(state);
+      node.Add(value, action, GetNode(newState ?? state));
+    }
+
     //
 
+    private readonly string initialState;
+    private readonly List<Node<T>> nodes;
     private Node<T> current;
+
+    private Node<T> GetNode(string state)
+    {
+      var node = nodes.FirstOrDefault(it => string.Compare(it.State, state, true, CultureInfo.InvariantCulture) == 0);
+      if (node == null)
+      {
+        node = new Node<T>(state);
+        nodes.Add(node);
+      }
+
+      return node;
+    }
   }
 }
--- FSM/FSM.ParseNumbers/Program.cs
+++ FSM/FSM.ParseNumbers/Program.cs
@@ -5,47 +5,92 @@ namespace Renfield.FSM.ParseNumbers
 {
   internal class Program
   {
+    private static FSM<char> fsm;
+    private static int sign;
+    private static double number;
+    private static double divisor;
+
     private static void Main(string[] args)
     {
-      var sign = 1;
-      var number = 0.0;
-      var divisor = 0.0;
+      fsm = new FSM<char>("start");
 
-      var start = new Node<char>("start");
-      var wholePart = new Node<char>("int part");
-      var fractPart = new Node<char>("decimal part");
+      fsm.Add("start", '+', null, "whole");
+      fsm.Add("start", '-', c => sign = -1, "whole");
+      fsm.Add("start", char.IsDigit, AddChar, "whole");
+      fsm.Add("start", '.', c => divisor = 0.1, "fract");
 
-      start.Add('+', null, wholePart);
-      start.Add('-', c => sign = -1, wholePart);
-      start.Add(char.IsDigit, c => number = number * 10 + (c - '0'), wholePart);
-      start.Add('.', c => divisor = 0.1, fractPart);
+      fsm.Add("whole", char.IsDigit, AddChar);
+      fsm.Add("whole", '.', c => divisor = 0.1, "fract");
 
-      wholePart.Add(char.IsDigit, c => number = number * 10 + (c - '0'));
-      wholePart.Add('.', c => divisor = 0.1, fractPart);
-
-      fractPart.Add(char.IsDigit, c =>
+      fsm.Add("fract", char.IsDigit, c =>
       {
         number += divisor * (c - '0');
         divisor /= 10;
       });
 
-      do
+      fsm.OnStart = () =>
       {
         sign = 1;
         number = 0.0;
         divisor = 0.0;
+      };
 
+      Verify();
+
+      do
+      {
         Console.Write("Enter a number: ");
         var s = Console.ReadLine();
         if (string.IsNullOrEmpty(s))
           break;
 
-        var fsm = new FSM<char>(start);
+        fsm.Restart();
         foreach (var c in s)
           fsm.Handle(c);
 
         Console.WriteLine("The number is " + sign * number);
       } while (true);
+    }
+
+    private static void AddChar(char c)
+    {
+      number = number * 10 + (c - '0');
+    }
+
+    private static void Verify()
+    {
+      AssertEquals(0.0, "");
+      AssertEquals(0.0, "0");
+      AssertEquals(0.0, "-0");
+      AssertEquals(0.0, "+0");
+      AssertEquals(0.0, "-00.00");
+      AssertEquals(0.0, "+00.00");
+      AssertEquals(0.1, ".1");
+      AssertEquals(0.1, "0.1");
+      AssertEquals(0.1, "00.1");
+      AssertEquals(0.1, "000.100");
+      AssertEquals(-0.1, "-.1");
+      AssertEquals(-0.1, "-0.1");
+      AssertEquals(-0.1, "-00.10");
+      AssertEquals(-12.34, "-12.34");
+      AssertEquals(12.34, "12.34");
+      AssertEquals(12.34, "+12.34");
+      AssertEquals(12.34, "+012.340");
+    }
+
+    private static void AssertEquals(double result, string s)
+    {
+      fsm.Restart();
+
+      foreach (var c in s)
+        fsm.Handle(c);
+
+      number *= sign;
+      if (Math.Abs(result - number) < 0.00001)
+        Console.WriteLine("[{0}] ok", s);
+      else
+        throw new Exception(string.Format("Assertion failed: parsing {0} resulted in {1} but was expecting {2}", s,
+          number, result));
     }
   }
 }
