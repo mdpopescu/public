af41656ab4070db1e769fc843b11461d8f3c6d8d
mdpopescu <mdpopescu@gmail.com>
2013-08-31 21:59:47 +0100

    simplified the TokenStream class

--- SimpleViewEngine/SimpleViewEngine.Library/Parsing/TokenStream.cs
+++ SimpleViewEngine/SimpleViewEngine.Library/Parsing/TokenStream.cs
@@ -14,21 +14,32 @@ namespace Renfield.SimpleViewEngine.Library.Parsing
 
     public TokenStream(IEnumerable<Token> tokens)
     {
+      rules = new Lazy<Dictionary<string, Func<string, Node>>>(CreateRules);
       this.tokens = tokens.ToList();
-      index = 0;
     }
 
     public IEnumerable<Node> Parse()
     {
+      index = 0;
+
       return InternalParse(Token.EOF);
     }
 
     //
 
-    private readonly List<Token> tokens;
-    private int index;
+    protected virtual Dictionary<string, Func<string, Node>> CreateRules()
+    {
+      return new Dictionary<string, Func<string, Node>>
+      {
+        {"constant", value => new ConstantNode(value)},
+        {"property", value => new PropertyNode(ExtractName(value, 2))},
+        {"if", value => new ConditionalNode(ExtractName(value, 5), InternalParse("endif"))},
+        {"else", value => new ElseNode()},
+        {"foreach", value => new RepeaterNode(ExtractName(value, 10), InternalParse("endfor"))},
+      };
+    }
 
-    private IEnumerable<Node> InternalParse(string eof)
+    protected IEnumerable<Node> InternalParse(string eof)
     {
       var nodes = new List<Node>();
 
@@ -45,58 +56,24 @@ namespace Renfield.SimpleViewEngine.Library.Parsing
       return nodes;
     }
 
-    private Node ReadNode(string type, string value)
-    {
-      switch (type)
-      {
-        case "constant":
-          return ReadConstantNode(value);
-
-        case "property":
-          return ReadPropertyNode(value);
-
-        case "if":
-          return ReadConditionalNode(value);
-
-        case "else":
-          return ReadElseNode();
-
-        case "foreach":
-          return ReadRepeaterNode(value);
-
-        default:
-          throw new Exception(string.Format("Unexpected token of type [{0}] and value [{1}]", type, value));
-      }
-    }
-
-    private static ConstantNode ReadConstantNode(string value)
-    {
-      return new ConstantNode(value);
-    }
-
-    private static PropertyNode ReadPropertyNode(string value)
-    {
-      return new PropertyNode(ExtractName(value, 2));
-    }
-
-    private static ElseNode ReadElseNode()
-    {
-      return new ElseNode();
-    }
-
-    private ConditionalNode ReadConditionalNode(string value)
-    {
-      return new ConditionalNode(ExtractName(value, 5), InternalParse("endif"));
-    }
-
-    private RepeaterNode ReadRepeaterNode(string value)
-    {
-      return new RepeaterNode(ExtractName(value, 10), InternalParse("endfor"));
-    }
-
-    private static string ExtractName(string value, int start)
+    protected static string ExtractName(string value, int start)
     {
       return value.Substring(start, value.Length - start - 2);
+    }
+
+    //
+
+    private readonly Lazy<Dictionary<string, Func<string, Node>>> rules;
+    private readonly List<Token> tokens;
+    private int index;
+
+    private Node ReadNode(string type, string value)
+    {
+      Func<string, Node> handler;
+      if (!rules.Value.TryGetValue(type, out handler))
+        throw new Exception(string.Format("Unexpected token of type [{0}] and value [{1}]", type, value));
+
+      return handler(value);
     }
   }
 }
