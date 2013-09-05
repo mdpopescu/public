01cafcb148a356a1ec207898a8a26ed85fb83725
mdpopescu <mdpopescu@gmail.com>
2013-09-05 03:20:30 +0100

    refactoring

---
+++ VinReader/VinReader.Console/ImageDecoder.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using ZXing;
+
+namespace Renfield.VinReader.Command
+{
+  public class ImageDecoder
+  {
+    public string Decode(Bitmap bmp)
+    {
+      return GenerateEffects(bmp)
+        .Select(InternalDecode)
+        .FirstOrDefault();
+    }
+
+    //
+
+    private const int SLICE_SIZE = 50;
+
+    private static IEnumerable<Bitmap> GenerateEffects(Bitmap bmp)
+    {
+      // each of the generated bitmaps will be disposed of by InternalDecode
+      yield return new Bitmap(bmp);
+
+      yield return Rotate(bmp, RotateFlipType.Rotate90FlipNone);
+      yield return Rotate(bmp, RotateFlipType.Rotate180FlipNone);
+      yield return Rotate(bmp, RotateFlipType.Rotate270FlipNone);
+
+      // split the image horizontally in "slices"
+      var y = 0;
+      while (y < bmp.Height)
+      {
+        var rect = new Rectangle(0, y, bmp.Width, SLICE_SIZE);
+        using (var slice = CropImage(bmp, rect))
+          yield return ToGrayscale(slice);
+
+        y += SLICE_SIZE;
+      }
+
+      // split the image vertically in "slices"
+      var x = 0;
+      while (x < bmp.Width)
+      {
+        var rect = new Rectangle(x, 0, SLICE_SIZE, bmp.Height);
+        using (var slice = CropImage(bmp, rect))
+          yield return ToGrayscale(slice);
+
+        x += SLICE_SIZE;
+      }
+    }
+
+    private static Bitmap Rotate(Image bmp, RotateFlipType flipType)
+    {
+      var bitmap = new Bitmap(bmp);
+      bitmap.RotateFlip(flipType);
+
+      return bitmap;
+    }
+
+    private static Bitmap CropImage(Image img, Rectangle cropArea)
+    {
+      var bitmap = new Bitmap(cropArea.Width, cropArea.Height);
+      using (var g = Graphics.FromImage(bitmap))
+      {
+        g.DrawImage(img, -cropArea.X, -cropArea.Y);
+        g.Save();
+
+        return bitmap;
+      }
+    }
+
+    private static Bitmap ToGrayscale(Image img)
+    {
+      var bitmap = new Bitmap(img.Width, img.Height);
+      using (var g = Graphics.FromImage(bitmap))
+      {
+        var cm = new ColorMatrix(new[]
+        {
+          new[] {0.3f, 0.3f, 0.3f, 0, 0},
+          new[] {0.59f, 0.59f, 0.59f, 0, 0},
+          new[] {0.11f, 0.11f, 0.11f, 0, 0},
+          new float[] {0, 0, 0, 1, 0, 0},
+          new float[] {0, 0, 0, 0, 1, 0},
+          new float[] {0, 0, 0, 0, 0, 1}
+        });
+
+        var ia = new ImageAttributes();
+        ia.SetColorMatrix(cm);
+        g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+        g.Save();
+
+        return bitmap;
+      }
+    }
+
+    private static string InternalDecode(Bitmap bmp)
+    {
+      var reader = new BarcodeReader();
+      var result = reader.Decode(bmp);
+      bmp.Dispose();
+
+      return result == null ? null : result.Text;
+    }
+  }
+}
--- VinReader/VinReader.Console/Program.cs
+++ VinReader/VinReader.Console/Program.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq;
-using ZXing;
 
 namespace Renfield.VinReader.Command
 {
   internal class Program
   {
-    private const int SLICE_HEIGHT = 50;
-
     private static void Main(string[] args)
     {
+      var decoder = new ImageDecoder();
+
       do
       {
         Console.Write("File name: ");
@@ -20,109 +16,12 @@ namespace Renfield.VinReader.Command
         if (string.IsNullOrEmpty(filename))
           break;
 
-        var result = TryDecode(filename);
-        Console.WriteLine(result ?? "Could not decode " + filename);
-      } while (true);
-    }
-
-    private static string TryDecode(string filename)
-    {
-      using (var bmp = (Bitmap) Image.FromFile(filename))
-      {
-        //var result = TryRotateAndDecode(bmp);
-        //if (result != null)
-        //  return result;
-
-        //// split the image horizontally in "slices", 50 pixels high, and try with each slices until one decodes
-        //var y = 0;
-        //while (y < bmp.Height)
-        //{
-        //  var rect = new Rectangle(0, y, bmp.Width, SLICE_HEIGHT);
-        //  var bitmap = ToGrayscale(CropImage(bmp, rect));
-        //  //bitmap.Save(Path.Combine(Path.GetDirectoryName(filename), string.Format("slice-{0}.jpg", y)), ImageFormat.Jpeg);
-
-        //  result = TryRotateAndDecode(bitmap);
-        //  if (result != null)
-        //    return result;
-
-        //  y += SLICE_HEIGHT;
-        //}
-
-        var codes = new ArrayList();
-        BarcodeImaging.FullScanPageCode39(ref codes, bmp, 50);
-        var candidate = codes
-          .Cast<string>()
-          .Select(it => new string(it.Where(char.IsLetterOrDigit).ToArray()))
-          .Select(it => it.StartsWith("I") ? it.Substring(1) : it)
-          .OrderByDescending(it => it.Length)
-          .FirstOrDefault();
-
-        return candidate;
-      }
-    }
-
-    private static string TryRotateAndDecode(Bitmap bmp)
-    {
-      for (var i = 1; i <= 4; i++)
-      {
-        var result = Decode(bmp);
-        if (result != null)
-          return result;
-
-        bmp = Rotate90(bmp);
-      }
-
-      return null;
-    }
-
-    private static Bitmap Rotate90(Image bmp)
-    {
-      var bitmap = new Bitmap(bmp.Width, bmp.Height);
-      bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
-
-      return bitmap;
-    }
-
-    private static Bitmap CropImage(Image img, Rectangle cropArea)
-    {
-      var bitmap = new Bitmap(cropArea.Width, cropArea.Height);
-      using (var g = Graphics.FromImage(bitmap))
-      {
-        g.DrawImage(img, -cropArea.X, -cropArea.Y);
-
-        return bitmap;
-      }
-    }
-
-    private static Bitmap ToGrayscale(Image img)
-    {
-      var bitmap = new Bitmap(img.Width, img.Height);
-      using (var g = Graphics.FromImage(bitmap))
-      {
-        var cm = new ColorMatrix(new[]
+        using (var bmp = (Bitmap) Image.FromFile(filename))
         {
-          new[] {0.3f, 0.3f, 0.3f, 0, 0},
-          new[] {0.59f, 0.59f, 0.59f, 0, 0},
-          new[] {0.11f, 0.11f, 0.11f, 0, 0},
-          new float[] {0, 0, 0, 1, 0, 0},
-          new float[] {0, 0, 0, 0, 1, 0},
-          new float[] {0, 0, 0, 0, 0, 1}
-        });
-
-        var ia = new ImageAttributes();
-        ia.SetColorMatrix(cm);
-        g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
-
-        return bitmap;
-      }
-    }
-
-    private static string Decode(Bitmap bmp)
-    {
-      var reader = new BarcodeReader();
-      var result = reader.Decode(bmp);
-
-      return result == null ? null : result.Text;
+          var result = decoder.Decode(bmp);
+          Console.WriteLine(result ?? "Could not decode " + filename);
+        }
+      } while (true);
     }
   }
 }
