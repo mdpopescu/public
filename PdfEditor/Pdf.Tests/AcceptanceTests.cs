20e621c38745d1bd26ed113be5cb07db2d7f82f5
mdpopescu <mdpopescu@gmail.com>
2013-10-12 12:17:56 +0100

    added sample form and "spikes" project

---
+++ PdfEditor/Pdf.Tests/AcceptanceTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pdf.Tests
+{
+  [TestClass]
+  public class AcceptanceTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
