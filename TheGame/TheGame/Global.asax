5c266724c2a5941326887955dbb7a03edd2171af
mdpopescu <mdpopescu@gmail.com>
2013-11-22 20:35:25 +0000

    Initial commit

---
+++ TheGame/TheGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheGame.MvcApplication" Language="C#" %>
---
+++ TheGame/TheGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheGame.Startup))]
+namespace TheGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
