94d56b024d38f0d39f2e25ec33d2cf0c9a1cc297
mdpopescu <mdpopescu@gmail.com>
2013-11-22 21:46:47 +0000

    Added a Game controller

---
+++ TheGame/TheGame/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheGame.Models;
+
+namespace TheGame.Controllers
+{
+  public class GameController : Controller
+  {
+    public ActionResult Create()
+    {
+      var guid = Guid.NewGuid().ToString();
+      return RedirectToAction("Join", new { id = guid });
+    }
+
+    public ActionResult Join(string id)
+    {
+      var model = new Game { Id = id };
+      return View(model);
+    }
+  }
+}
---
+++ TheGame/TheGame/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheGame.Models
+{
+  public class Game
+  {
+    public string Id { get; set; }
+  }
+}
