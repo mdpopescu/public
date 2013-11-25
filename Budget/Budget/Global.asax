244fea6625d843e75fd6dbe40eb4b345b81c6bd1
mdpopescu <mdpopescu@gmail.com>
2013-11-25 22:13:43 +0000

    Initial commit

---
+++ Budget/Budget/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Budget.MvcApplication" Language="C#" %>
---
+++ Budget/Budget/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Budget.Startup))]
+namespace Budget
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
