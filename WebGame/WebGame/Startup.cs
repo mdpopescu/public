67dfc210d0c8cda108375c6510a30280db900f74
Marcel Popescu <mdpopescu@gmail.com>
2014-09-21 06:33:44 +0300

    Initial commit

---
+++ WebGame/WebGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGame.Startup))]
+namespace WebGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
