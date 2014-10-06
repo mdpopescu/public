5ee6e58b39841f6f1d38e4836492855225f3e4ed
Marcel Popescu <mdpopescu@gmail.com>
2014-10-06 11:50:07 +0300

    Changed the code to hide the app from the taskbar

--- DesktopClock/DesktopClock/MainWindow.xaml.cs
+++ DesktopClock/DesktopClock/MainWindow.xaml.cs
@@ -1,37 +1,31 @@
 ﻿using System;
 using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace DesktopClock
 {
-    /// <summary>
-    ///     Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  ///   Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+      InitializeComponent();
 
-            timer = new Timer(_ => UpdateClock(), null, 0, 1000);
-        }
-
-        //
-
-        private readonly Timer timer;
-
-        private void UpdateClock()
-        {
-            var text = DateTime.Now.ToString("dd MMM yyyy  HH:mm:ss");
-            this.UIChange(_ => LblClock.Text = text, text);
-        }
-
-        //
-
-        private void Window_Initialized(object sender, System.EventArgs e)
-        {
-            //LblClock.Margin = new Thickness(0, 0, 0, 0);
-        }
+      timer = new Timer(_ => UpdateClock(), null, 0, 1000);
     }
+
+    //
+
+    private readonly Timer timer;
+
+    private void UpdateClock()
+    {
+      var text = DateTime.Now.ToString("dd MMM yyyy  HH:mm:ss");
+      this.UIChange(_ => LblClock.Text = text, text);
+    }
+
+    //
+  }
 }
