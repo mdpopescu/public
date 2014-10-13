cbdf9a674a85ebb45a9091f8847ac9167f30805b
Marcel Popescu <mdpopescu@gmail.com>
2014-10-13 13:37:07 +0300

    How to detect the maximum depth of recursion allowed (re: Karl Seguin at https://twitter.com/karlseguin/status/521592595282006016 )

---
+++ MaxRecursion/MaxRecursion/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace MaxRecursion
+{
+  internal class Program
+  {
+    private static readonly string appPath = Assembly.GetExecutingAssembly().Location;
+
+    private static void Main(string[] args)
+    {
+      if (args.Length > 0)
+      {
+        Console.WriteLine("Trying with " + args[0]);
+        RecursiveMethod(int.Parse(args[0]));
+      }
+      else
+      {
+        var result = BinarySearch(1, int.MaxValue, RecursiveMethod);
+        Console.WriteLine(result);
+      }
+    }
+
+    private static int BinarySearch(int min, int max, Action<int> action)
+    {
+      var l = min;
+      var r = max;
+
+      while (l <= r)
+      {
+        var m = l / 2 + r / 2;
+        if (TryFunc(m))
+          l = m + 1;
+        else
+          r = m - 1;
+      }
+
+      return l;
+    }
+
+    private static bool TryFunc(int n)
+    {
+      var process = Process.Start(appPath, n.ToString());
+      process.WaitForExit();
+      return process.ExitCode == 0;
+    }
+
+    private static void RecursiveMethod(int n)
+    {
+      if (n <= 0)
+        return;
+
+      RecursiveMethod(n - 1);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -23,6 +23,8 @@ Public repository - CC-by-SA 3.0
 
 * Inventory: (INCOMPLETE) inventory management system - mainly to use SignalR and Knockout.js
 
+* MaxRecursion: answer to a problem posed by Karl Seguin on Twitter https://twitter.com/karlseguin/status/521592595282006016 - Write code that prints out the maximum depth of recursion allowed.
+
 * PageFaults: project from https://www.freelancer.com/projects/C-Sharp-Programming/FIFO-OPTIMAL-LRU.html (I included the detailed spec in the project); apparently someone else is taking the same exam - https://www.freelancer.com/projects/C-Sharp-Programming/lab-rep-project-repost.4718106.html
 
 * QRMaker: the same guy with the Yelp project had another one, a bulk QR generator; unfortunately I didn't get this job either - https://www.odesk.com/jobs/URGENT-Really-fast-Bulk-maker-zip_~01368c1f8e16aa230d
