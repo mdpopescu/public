58c9234f23080c714e419f71510092ac30a097bc
Marcel Popescu <mdpopescu@gmail.com>
2014-10-13 23:02:58 +0300

    The label is created at runtime; using Rx instead of an explicit timer

--- DesktopClock/DesktopClock/MainWindow.xaml.cs
+++ DesktopClock/DesktopClock/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
-using System.Threading;
+using System.Reactive.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace DesktopClock
 {
@@ -13,17 +16,30 @@ namespace DesktopClock
     {
       InitializeComponent();
 
-      timer = new Timer(_ => UpdateClock(), null, 0, 1000);
+      var label = new TextBlock
+      {
+        Text = "dd MMM yyyy  HH:mm:ss",
+        FontFamily = new FontFamily("Bookman Old Style"),
+        FontSize = 72,
+        VerticalAlignment = VerticalAlignment.Top,
+        HorizontalAlignment = HorizontalAlignment.Center,
+        TextAlignment = TextAlignment.Center,
+        Effect = new DropShadowEffect(),
+      };
+      Main.Children.Add(label);
+
+      Observable
+        .Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1))
+        .ObserveOn(this)
+        .Subscribe(_ => UpdateClock(label));
     }
 
     //
 
-    private readonly Timer timer;
-
-    private void UpdateClock()
+    private void UpdateClock(TextBlock label)
     {
       var text = DateTime.Now.ToString("dd MMM yyyy  HH:mm:ss");
-      this.UIChange(_ => LblClock.Text = text, text);
+      label.Text = text;
     }
 
     //
