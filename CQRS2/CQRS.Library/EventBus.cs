e76a7966fd9698b3177ac9e6b29cfe3957d792e6
Marcel Popescu <mdpopescu@gmail.com>
2015-04-22 09:37:24 +0300

    Objects can subscribe to events

---
+++ CQRS2/CQRS.Library/EventBus.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CQRS.Library
+{
+  public static class EventBus
+  {
+    public static void Subscribe(object obj, params string[] names)
+    {
+      foreach (var name in names)
+        AddSubscription(obj, name);
+    }
+
+    public static void Send(string name, params object[] args)
+    {
+      ConcurrentBag<object> list;
+      if (!subscriptions.TryGetValue(name, out list))
+        return;
+
+      Parallel.ForEach(list.AsEnumerable(), target => Send(target, name, args));
+    }
+
+    //
+
+    // ReSharper disable InconsistentNaming
+    private static readonly ConcurrentDictionary<string, ConcurrentBag<object>> subscriptions = new ConcurrentDictionary<string, ConcurrentBag<object>>();
+    private static readonly object subscriptionsLock = new object();
+    // ReSharper enable InconsistentNaming
+
+    private static void AddSubscription(object obj, string name)
+    {
+      lock (subscriptionsLock)
+      {
+        var list = subscriptions.GetOrAdd(name, _ => new ConcurrentBag<object>());
+        list.Add(obj);
+        subscriptions[name] = list;
+      }
+    }
+
+    private static void Send(object target, string name, params object[] args)
+    {
+      var method = FindMethod(target, name);
+      if (method != null)
+        method.Invoke(target, args);
+    }
+
+    private static MethodInfo FindMethod(object target, string name)
+    {
+      var type = target.GetType();
+      return type.GetMethod(name, BindingFlags.Instance | BindingFlags.Public);
+    }
+  }
+}
---
+++ CQRS2/CQRS.Tests/EventBusTests.cs
@@ -0,0 +1,36 @@
+﻿using CQRS.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CQRS.Tests
+{
+  [TestClass]
+  public class EventBusTests
+  {
+    [TestMethod]
+    public void ObjectsCanSubscribeToEvents()
+    {
+      var obj = new MyClass1();
+
+      EventBus.Send("E1", 5);
+
+      Assert.AreEqual(5, obj.X);
+    }
+
+    //
+
+    private class MyClass1
+    {
+      public int X { get; private set; }
+
+      public MyClass1()
+      {
+        EventBus.Subscribe(this, "E1");
+      }
+
+      public void E1(int x)
+      {
+        X = x;
+      }
+    }
+  }
+}
