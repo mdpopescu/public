70b75f22ff50ea6682e9ffe6f1b2c7fdf6b900f3
Marcel Popescu <mdpopescu@gmail.com>
2015-05-20 11:31:09 +0300

    First acceptance test

--- httpd/httpd.Tests/AcceptanceTests.cs
+++ httpd/httpd.Tests/AcceptanceTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace httpd.Tests
 {
@@ -6,8 +7,29 @@ namespace httpd.Tests
   public class AcceptanceTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void ReturnsAnExistingFile()
     {
+      var basePath = @"S:\GIT-public\httpd";
+      var port = Sys.FindAvailablePort();
+
+      var path = basePath + @"\httpd\bin\debug\httpd.exe";
+      var args = string.Format(@"{0} {1}\httpd.Tests\Web", port, basePath);
+
+      var process = Sys.Run(path, args);
+      try
+      {
+        using (var web = new WebClient())
+        {
+          web.BaseAddress = "http://localhost:" + port;
+
+          var response = web.DownloadString("one-byte.txt");
+          Assert.AreEqual("z", response);
+        }
+      }
+      finally
+      {
+        process.Kill();
+      }
     }
   }
 }
--- httpd/httpd/Program.cs
+++ httpd/httpd/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace httpd
+﻿using System;
+
+namespace httpd
 {
   internal static class Program
   {
     private static void Main(string[] args)
     {
+      Console.ReadLine();
     }
   }
 }
