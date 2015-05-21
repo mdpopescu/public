51cd4d329e4957b9c98d8007f718a522906ea2e8
Marcel Popescu <mdpopescu@gmail.com>
2015-05-21 15:25:24 +0300

    Added the time changes to the acceptance tests

--- SocialNetwork/SocialNetwork.Tests/AcceptanceTests.cs
+++ SocialNetwork/SocialNetwork.Tests/AcceptanceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SocialNetwork.Library.Services;
 
@@ -20,6 +21,8 @@ namespace SocialNetwork.Tests
     {
       Scenario1_Posting();
 
+      Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 0);
+
       var response1 = sut.Read("Alice").ToList();
       Assert.AreEqual(1, response1.Count);
       Assert.AreEqual("I love the weather today (5 minutes ago)", response1[0]);
@@ -35,7 +38,10 @@ namespace SocialNetwork.Tests
     {
       Scenario1_Posting();
 
+      Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 0);
       sut.Post("Charlie", "I'm in New York today! Anyone want to have a coffee?");
+
+      Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 2);
 
       sut.Follow("Charlie", "Alice");
       var response1 = sut.Wall("Charlie").ToList();
@@ -56,8 +62,13 @@ namespace SocialNetwork.Tests
 
     private void Scenario1_Posting()
     {
+      Sys.Time = () => new DateTime(2000, 1, 1, 10, 0, 0);
       sut.Post("Alice", "I love the weather today");
+
+      Sys.Time = () => new DateTime(2000, 1, 1, 10, 3, 0);
       sut.Post("Bob", "Damn! We lost!");
+
+      Sys.Time = () => new DateTime(2000, 1, 1, 10, 4, 0);
       sut.Post("Bob", "Good game though.");
     }
   }
