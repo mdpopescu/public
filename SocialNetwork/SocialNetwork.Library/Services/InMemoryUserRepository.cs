bfd0ce678bc0a0828867d36a32c9cb328193f36a
Marcel Popescu <mdpopescu@gmail.com>
2015-05-21 15:57:13 +0300

    Added (non-working) implementation of the UserRepository interface and changed the acceptance test to use it

---
+++ SocialNetwork/SocialNetwork.Library/Services/InMemoryUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SocialNetwork.Library.Contracts;
+
+namespace SocialNetwork.Library.Services
+{
+  public class InMemoryUserRepository : UserRepository
+  {
+    public void AddFollower(string user, string other)
+    {
+      //
+    }
+  }
+}
--- SocialNetwork/SocialNetwork.Tests/AcceptanceTests.cs
+++ SocialNetwork/SocialNetwork.Tests/AcceptanceTests.cs
@@ -13,7 +13,7 @@ namespace SocialNetwork.Tests
     [TestInitialize]
     public void SetUp()
     {
-      sut = new CommandHandler(new InMemoryMessageRepository(), null, new TimeFormatter());
+      sut = new CommandHandler(new InMemoryMessageRepository(), new InMemoryUserRepository(), new TimeFormatter());
     }
 
     [TestMethod]
