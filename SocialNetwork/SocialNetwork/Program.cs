273550e5a28e94ae1af85efc71c0e3133d10b152
Marcel Popescu <mdpopescu@gmail.com>
2015-05-21 16:32:52 +0300

    The console application works

--- SocialNetwork/SocialNetwork/Program.cs
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using SocialNetwork.Library.Services;
 
 namespace SocialNetwork
 {
-  class Program
+  internal static class Program
   {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+      var handler = new CommandHandler(new InMemoryMessageRepository(), new InMemoryUserRepository(), new TimeFormatter());
+
+      Console.WriteLine("Social Network");
+      Console.WriteLine();
+
+      while (true)
+      {
+        Console.Write("> ");
+
+        var line = Console.ReadLine();
+        if (line == null)
+          break;
+
+        var parts = line.Split(' ');
+        var user = parts[0];
+
+        if (parts.Length == 1)
+          ShowLines(handler.Read(user));
+        else if (parts[1] == "->")
+          handler.Post(user, string.Join(" ", parts.Skip(2)));
+        else if (parts[1] == "follows")
+          handler.Follow(user, parts[2]);
+        else if (parts[1] == "wall")
+          ShowLines(handler.Wall(user));
+      }
+    }
+
+    private static void ShowLines(IEnumerable<string> lines)
+    {
+      foreach (var line in lines)
+        Console.WriteLine(line);
     }
   }
 }
