bde4cd0c23431fa6cc6dcf1f45af1daacf767809
Marcel Popescu <mdpopescu@gmail.com>
2015-05-28 12:10:45 +0300

    Test refactoring

--- ISBN/ISBN.Tests/ConverterTests.cs
+++ ISBN/ISBN.Tests/ConverterTests.cs
@@ -6,24 +6,32 @@ namespace ISBN.Tests
   [TestClass]
   public class ConverterTests
   {
+    private Converter sut;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+      sut = new Converter();
+    }
+
     [TestMethod]
     public void Test1()
     {
-      var sut = new Converter();
-
-      var result = sut.Convert("978155192370");
-
-      Assert.AreEqual("155192370x", result);
+      Check("978155192370", "155192370x");
     }
 
     [TestMethod]
     public void Test2()
     {
-      var sut = new Converter();
+      Check("978140007917", "1400079179");
+    }
 
-      var result = sut.Convert("978140007917");
+    //
 
-      Assert.AreEqual("1400079179", result);
+    private void Check(string given, string expected)
+    {
+      var result = sut.Convert(given);
+      Assert.AreEqual(expected, result);
     }
   }
 }
