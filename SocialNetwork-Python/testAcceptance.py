ff3090aa635135c161f5d2b4a2d19ff0118349e8
Marcel Popescu <mdpopescu@gmail.com>
2015-06-09 20:44:17 +0100

    changed the acceptance test to account for time

--- SocialNetwork-Python/testAcceptance.py
+++ SocialNetwork-Python/testAcceptance.py
@@ -2,8 +2,11 @@ __author__ = 'marcel'
 
 import unittest
 
+import datetime
+
 from Storage import Storage
 from Formatter import Formatter
+from Clock import Clock
 from App import App
 
 class testAcceptance(unittest.TestCase):
@@ -14,18 +17,23 @@ class testAcceptance(unittest.TestCase):
         self.sut = App(storage, formatter)
 
     def test_1(self):
+        Clock.now = staticmethod(lambda: datetime.datetime(2000, 1, 2, 3, 4, 5))
         self.sut.post("Marcel", "Hello World!")
+        Clock.now = staticmethod(lambda: datetime.datetime(2000, 1, 2, 3, 5, 5))
         self.sut.post("Marcel", "How is everyone?")
 
+        Clock.now = staticmethod(lambda: datetime.datetime(2000, 1, 2, 3, 6, 5))
         lines = self.sut.get("Marcel")
 
         self.assertEquals(2, len(lines))
         self.assertEquals("How is everyone? (1 minute ago)", lines[0])
         self.assertEquals("Hello World! (2 minutes ago)", lines[1])
 
+        Clock.now = staticmethod(lambda: datetime.datetime(2000, 1, 2, 3, 6, 5))
         self.sut.post("Anca", "We're doing great!")
         self.sut.follows("Marcel", "Anca")
 
+        Clock.now = staticmethod(lambda: datetime.datetime(2000, 1, 2, 3, 6, 10))
         lines = self.sut.wall("Marcel")
 
         self.assertEquals(3, len(lines))
