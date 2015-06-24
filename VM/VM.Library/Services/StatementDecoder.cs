9ee5a80cb6300bee2b044c18f416acb205daf41a
Marcel Popescu <mdpopescu@gmail.com>
2015-06-24 10:57:06 +0300

    The decoder can execute the CLR statement

---
+++ VM/VM.Library/Services/StatementDecoder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using VM.Library.Contracts;
+using VM.Library.Models;
+
+namespace VM.Library.Services
+{
+  public class StatementDecoder : Decoder
+  {
+    public StatementDecoder()
+    {
+      actions = new Dictionary<byte, Action<State>>();
+
+      actions[0x08] = state => state.Registers[0] = 0;
+      actions[0x09] = state => state.Registers[1] = 0;
+      actions[0x0A] = state => state.Registers[2] = 0;
+      actions[0x0B] = state => state.Registers[3] = 0;
+      actions[0x0C] = state => state.Registers[4] = 0;
+      actions[0x0D] = state => state.Registers[5] = 0;
+      actions[0x0E] = state => state.Registers[6] = 0;
+      actions[0x0F] = state => state.Registers[7] = 0;
+    }
+
+    public void Execute(State state, params byte[] bytes)
+    {
+      var code = bytes[0];
+      if (!actions.ContainsKey(code))
+        return;
+
+      actions[code](state);
+    }
+
+    //
+
+    private readonly Dictionary<byte, Action<State>> actions;
+  }
+}
---
+++ VM/VM.Tests/Services/StatementDecoderTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VM.Library.Models;
+using VM.Library.Services;
+
+namespace VM.Tests.Services
+{
+  [TestClass]
+  public class StatementDecoderTests
+  {
+    private StatementDecoder sut;
+    private State state;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+      sut = new StatementDecoder();
+      state = new State
+      {
+        Memory = new byte[65536],
+        Registers = new ushort[8],
+        StackPointer = 0,
+        ProgramCounter = 0,
+      };
+    }
+
+    [TestMethod]
+    public void ClearsTheRegisters()
+    {
+      for (var r = 0; r <= 7; r++)
+      {
+        state.Registers[r] = 1;
+
+        sut.Execute(state, (byte) (0x08 + r));
+
+        Assert.AreEqual(0, state.Registers[r]);
+      }
+    }
+  }
+}
