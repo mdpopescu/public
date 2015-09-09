1fe53e263d39587605fa2e625c0417a410837b32
Bogdan Anghel <13angel@gmail.com>
2015-09-09 20:57:05 +0100

    added Library project, FileInformation & DirectoryInformation classes

---
+++ SyncMaster/SyncMaster.Library/Services/DirectoryFileSystem.cs
@@ -0,0 +1,30 @@
+﻿using SyncMaster.Library.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SyncMaster.Library.Models;
+using System.IO;
+
+namespace SyncMaster.Library.Services
+{
+    public class DirectoryFileSystem : DirectoryRepository
+    {
+        public DirectoryInformation BuildDirectoryInfo(string path)
+        {
+            var result = new DirectoryInformation();
+            result.FullPath = path;
+
+            var files = Directory.GetFiles(path, "*.", SearchOption.TopDirectoryOnly);
+            foreach (var file in files)
+                result.Files.Add(new FileInformation(file));
+
+            var folders = Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly);
+            foreach (var folder in folders)
+                result.Directories.Add(BuildDirectoryInfo(folder));
+
+            return result;
+        }
+    }
+}
--- SyncMaster/SyncMaster/Program.cs
+++ SyncMaster/SyncMaster/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace SyncMaster
+﻿using SyncMaster.Library.Models;
+using SyncMaster.Library.Repository;
+using SyncMaster.Library.Services;
+
+namespace SyncMaster
 {
   internal class Program
   {
     private static void Main(string[] args)
     {
+       DirectoryRepository directoryRepository = new DirectoryFileSystem();
+       string rootPath = "";
+       foreach(var arg in args)
+        {
+            if (arg.StartsWith("--path="))
+            {
+                rootPath = arg.Replace("--path=", "");
+            }
+        }
+
+        var rootFolder = directoryRepository.BuildDirectoryInfo(rootPath);
     }
   }
 }
