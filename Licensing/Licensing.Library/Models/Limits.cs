8f1dbf10aaa07a7e091e672d9a75559318444d21
Marcel Popescu <mdpopescu@gmail.com>
2015-09-15 19:58:43 +0100

    Added a GetRemainingDays method to the Limits class

--- Licensing/Licensing.Library/Models/Limits.cs
+++ Licensing/Licensing.Library/Models/Limits.cs
@@ -7,5 +7,16 @@ namespace Renfield.Licensing.Library.Models
   {
     public int Days { get; set; }
     public int Runs { get; set; }
+
+    public int GetRemainingDays(DateTime createdOn)
+    {
+      if (Days < 0)
+        return int.MaxValue;
+
+      var elapsed = (int) Math.Round(DateTime.Today.Subtract(createdOn).TotalDays);
+      var remaining = Days - elapsed;
+
+      return remaining <= 0 ? 0 : remaining;
+    }
   }
 }
---
+++ Licensing/Licensing.Tests/Models/LimitsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Renfield.Licensing.Library.Models;
+
+namespace Renfield.Licensing.Tests.Models
+{
+  [TestClass]
+  public class LimitsTests
+  {
+    [TestClass]
+    public class GetRemainingDays : LimitsTests
+    {
+      [TestMethod]
+      public void ReturnsMaxValueIfLimitDaysIsMinusOne()
+      {
+        var sut = new Limits {Days = -1};
+
+        var result = sut.GetRemainingDays(DateTime.Today);
+
+        Assert.AreEqual(int.MaxValue, result);
+      }
+
+      [TestMethod]
+      public void ReturnsZeroIfExpired()
+      {
+        var sut = new Limits {Days = 5};
+
+        var result = sut.GetRemainingDays(new DateTime(2000, 1, 1));
+
+        Assert.AreEqual(0, result);
+      }
+
+      [TestMethod]
+      public void ReturnsNumberOfRemainingDays()
+      {
+        var sut = new Limits {Days = 5};
+
+        var result = sut.GetRemainingDays(DateTime.Today.AddDays(-2));
+
+        Assert.AreEqual(3, result); // includes today
+      }
+    }
+  }
+}
