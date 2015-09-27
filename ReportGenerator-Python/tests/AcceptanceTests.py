2d1494c529d71b49f129f40adc7678965d57fcfd
Marcel Popescu <mdpopescu@gmail.com>
2015-09-27 10:47:19 +0100

    More tests

--- ReportGenerator-Python/tests/AcceptanceTests.py
+++ ReportGenerator-Python/tests/AcceptanceTests.py
@@ -8,53 +8,68 @@ class AcceptanceTests(unittest.TestCase):
     def setUp(self):
         self.settings = {
             "sample": {
-                "query": "SELECT * FROM data"
+                "query": "SELECT * FROM data",
+                "sender": "report_sender@baml.com",
+                "recipients": ["report_target@baml.com"],
+                "subject": "Reports for 2000-01-02",
+                "header": """<html>
+                    <head>
+                        <title>Reports for {date}</title>
+                    </head>
+                    <body>
+                        <table>
+                """,
+                "footer": """</tbody>
+                        </table>
+                    </body>
+                    </html>
+                """
             }
         }
 
         table = MagicMock()
-        table.Columns = ['a', 'b', 'c']
+        table.Columns = ["a", "b", "c"]
         table.Rows = [
-            [1, 'x', 'stuff'],
-            [2, 'y', 'more stuff']
+            [1, "x", "stuff"],
+            [2, "y", "more stuff"]
         ]
 
         self.dataReader = MagicMock()
-        self.dataReader.Read.return_value=table
-
+        self.dataReader.Read.return_value = table
+        self.formatter = MagicMock()
         self.notifier = MagicMock()
 
-        self.sut = ReportGenerator(self.settings, self.dataReader)
-
-        # overwrite the notifier - I don't want to actually send an email
-        self.sut.notifier = self.notifier
+        self.sut = ReportGenerator(self.settings, self.dataReader, self.formatter, self.notifier)
 
     def test_run(self):
         expected = """<html>
-        <body>
-            <table>
-                <thead>
-                    <th>a</th>
-                    <th>b</th>
-                    <th>c</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>x</td>
-                        <td>stuff</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>y</td>
-                        <td>more stuff</td>
-                    </tr>
-                </tbody>
-            </table>
-        </body>
-        </html>
+            <head>
+                <title>Reports for 2000-01-02</title>
+            </head>
+            <body>
+                <table>
+                    <thead>
+                        <th>a</th>
+                        <th>b</th>
+                        <th>c</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>x</td>
+                            <td>stuff</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>y</td>
+                            <td>more stuff</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+            </html>
         """
 
         self.sut.Run("sample")
 
-        self.notifier.Notify.assert_called_with('report_sender@baml.com', ['report_target@baml.com'], 'Reports for 2000-01-02', expected)
+        self.notifier.Notify.assert_called_with("report_sender@baml.com", ["report_target@baml.com"], "Reports for 2000-01-02", expected)
--- ReportGenerator-Python/tests/ReportGeneratorTests.py
+++ ReportGenerator-Python/tests/ReportGeneratorTests.py
@@ -8,12 +8,17 @@ class ReportGeneratorTests(unittest.TestCase):
     def setUp(self):
         self.settings = {
             "sample": {
-                "query": "SELECT * FROM data"
+                "query": "SELECT * FROM data",
+                "sender": "report_sender@baml.com",
+                "recipients": ["report_target@baml.com"],
+                "subject": "Reports for 2000-01-02"
             }
         }
         self.dataReader = MagicMock()
+        self.formatter = MagicMock()
+        self.notifier = MagicMock()
 
-        self.sut = ReportGenerator(self.settings, self.dataReader)
+        self.sut = ReportGenerator(self.settings, self.dataReader, self.formatter, self.notifier)
 
     def test_RunReadsFromTheDatabase(self):
         self.sut.Run("sample")
@@ -22,3 +27,42 @@ class ReportGeneratorTests(unittest.TestCase):
 
     def test_RunThrowsIfReportUnknown(self):
         self.assertRaises(Exception, self.sut.Run, "unknown")
+
+    def test_RunFormatsTheData(self):
+        table = MagicMock()
+        table.Columns = ["a", "b", "c"]
+        table.Rows = [
+            [1, "x", "stuff"],
+            [2, "y", "more stuff"]
+        ]
+        self.dataReader.Read.return_value = table
+
+        self.sut.Run("sample")
+
+        self.formatter.Format.assert_called_with(table)
+
+    def test_RunDoesNotFormatTheDataIfNoRows(self):
+        table = MagicMock()
+        table.Columns = ["a", "b", "c"]
+        table.Rows = []
+
+        self.dataReader.Read.return_value = table
+
+        self.sut.Run("sample")
+
+        self.assertEquals(0, self.formatter.Format.call_count)
+
+    def test_RunSendsNotification(self):
+        table = MagicMock()
+        table.Columns = ["a", "b", "c"]
+        table.Rows = [
+            [1, "x", "stuff"],
+            [2, "y", "more stuff"]
+        ]
+        self.dataReader.Read.return_value = table
+        self.formatter.Format.return_value = "abc"
+
+        self.sut.Run("sample")
+
+        self.notifier.Send.assert_called_with("report_sender@baml.com", ["report_target@baml.com"], "Reports for 2000-01-02", "abc")
+        
