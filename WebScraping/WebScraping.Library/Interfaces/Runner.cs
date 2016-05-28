0f5d3e5a283d6db1985fcd350b60a60b85ce553c
Marcel Popescu <mdpopescu@gmail.com>
2016-05-28 16:23:37 +0300

    Initial commit - first acceptance test

---
+++ WebScraping/WebScraping.Library/Interfaces/Runner.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace WebScraping.Library.Interfaces
+{
+    public class Runner
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="Runner" /> class.
+        /// </summary>
+        /// <param name="input">The source of the user input.</param>
+        /// <param name="output">The program output.</param>
+        public Runner(TextReader input, TextWriter output)
+        {
+            //
+        }
+
+        /// <summary>
+        ///     Runs a program.
+        /// </summary>
+        /// <param name="program">The program to run.</param>
+        public void Run(string program)
+        {
+            //
+        }
+    }
+}
---
+++ WebScraping/WebScraping.Tests/AcceptanceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebScraping.Library.Interfaces;
+
+namespace WebScraping.Tests
+{
+    [TestClass]
+    public class AcceptanceTests
+    {
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            const string PROGRAM = "print 'Hello, world'";
+
+            using (var input = new StringReader(""))
+            {
+                var sb = new StringBuilder();
+
+                using (var output = new StringWriter(sb))
+                {
+                    var sut = new Runner(input, output);
+
+                    sut.Run(PROGRAM);
+                }
+
+                Assert.AreEqual("Hello, world\r\n", sb.ToString());
+            }
+        }
+    }
+}
