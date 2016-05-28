e445898046f2015db596888644e8c7d4be5a3a12
Marcel Popescu <mdpopescu@gmail.com>
2016-05-28 17:09:36 +0300

    The compiler can handle simple print statements

--- WebScraping/WebScraping.Library/Implementations/MultiStepCompiler.cs
+++ WebScraping/WebScraping.Library/Implementations/MultiStepCompiler.cs
@@ -1,12 +1,57 @@
-﻿using WebScraping.Library.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebScraping.Library.Interfaces;
 
 namespace WebScraping.Library.Implementations
 {
     public class MultiStepCompiler : Compiler
     {
+        public MultiStepCompiler()
+        {
+            stages = new List<Func<string[], string[]>>
+            {
+                Stage1,
+            };
+        }
+
         public string Compile(string program)
         {
-            return "";
+            var lines = program
+                .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.Trim())
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .ToArray();
+            var result = stages.Aggregate(lines, (current, stage) => stage(current));
+
+            var csProgram = string.Join(Environment.NewLine, result);
+            if (!string.IsNullOrWhiteSpace(csProgram))
+                csProgram += Environment.NewLine;
+
+            return csProgram;
+        }
+
+        //
+
+        private readonly List<Func<string[], string[]>> stages;
+
+        private static string[] Stage1(string[] lines)
+        {
+            var result = new List<string>();
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                result.Add($"//{(i + 1)}//");
+
+                var line = lines[i];
+                if (line.IndexOf("print ", StringComparison.OrdinalIgnoreCase) != 0)
+                    continue;
+
+                line = "Console.WriteLine(" + line.Substring(6) + ");";
+                result.Add(line);
+            }
+
+            return result.ToArray();
         }
     }
 }
---
+++ WebScraping/WebScraping.Tests/Implementations/MultiStepCompilerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebScraping.Library.Implementations;
+
+namespace WebScraping.Tests.Implementations
+{
+    [TestClass]
+    public class MultiStepCompilerTests
+    {
+        private MultiStepCompiler sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new MultiStepCompiler();
+        }
+
+        [TestMethod]
+        public void ReturnsAnEmptyStringIfProgramIsEmpty()
+        {
+            var result = sut.Compile("  ");
+
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void ConvertsThePrintStatementToConsoleWriteLine()
+        {
+            var result = sut.Compile("print 1");
+
+            Assert.AreEqual("//1//\r\nConsole.WriteLine(1);\r\n", result);
+        }
+    }
+}
