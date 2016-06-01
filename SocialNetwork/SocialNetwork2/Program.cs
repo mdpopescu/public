66860a30146188cf1587a28b348fdf7d063fdece
Marcel Popescu <mdpopescu@gmail.com>
2016-06-01 09:02:24 +0300

    Added a new console app using the second version

---
+++ SocialNetwork/SocialNetwork2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SocialNetwork2.Library.Implementations;
+
+namespace SocialNetwork2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var userRepository = new UserRepository();
+            var commandHandler = new CommandHandler(userRepository);
+
+            Console.WriteLine("Social Network");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("> ");
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+
+                var output = commandHandler.Handle(input);
+                foreach (var line in output)
+                    Console.WriteLine(line);
+            }
+        }
+    }
+}
