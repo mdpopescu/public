e09ecf9e1014b365022c1f1d8a31c9c27bec155e
Marcel Popescu <mdpopescu@gmail.com>
2016-06-01 07:45:50 +0300

    The command handler sends POSTs to the appropriate user

--- SocialNetwork/SocialNetwork2.Library/Implementations/CommandHandler.cs
+++ SocialNetwork/SocialNetwork2.Library/Implementations/CommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SocialNetwork2.Library.Implementations
@@ -7,12 +8,24 @@ namespace SocialNetwork2.Library.Implementations
     {
         public CommandHandler(UserRepository userRepository)
         {
-            //
+            this.userRepository = userRepository;
         }
 
         public IEnumerable<string> Handle(string command)
         {
+            var index = command.IndexOf("->", StringComparison.Ordinal);
+            if (index > 0)
+            {
+                var userName = command.Substring(0, index - 1);
+                var user = userRepository.CreateOrFind(userName);
+                user.Post(command.Substring(index + 3));
+            }
+
             return Enumerable.Empty<string>();
         }
+
+        //
+
+        private readonly UserRepository userRepository;
     }
 }
---
+++ SocialNetwork/SocialNetwork2.Tests/Implementations/CommandHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork2.Library.Implementations;
+
+namespace SocialNetwork2.Tests.Implementations
+{
+    [TestClass]
+    public class CommandHandlerTests
+    {
+        private UserRepository userRepository;
+
+        private CommandHandler sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            userRepository = new UserRepository();
+            sut = new CommandHandler(userRepository);
+        }
+
+        [TestMethod]
+        public void PostingAddsMessagesToUser()
+        {
+            sut.Handle("abc -> test");
+
+            var user = userRepository.CreateOrFind("abc");
+            var messages = user.Read().ToList();
+            Assert.AreEqual(1, messages.Count);
+            Assert.IsTrue(messages[0].StartsWith("test"));
+        }
+    }
+}
