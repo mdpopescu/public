4e37f1ce84a1e2c517047707dd53ea68d06c4be8
Marcel Popescu <mdpopescu@gmail.com>
2016-06-01 07:39:23 +0300

    Added a new acceptance test, using the command handler

---
+++ SocialNetwork/SocialNetwork2.Library/Implementations/CommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialNetwork2.Library.Implementations
+{
+    public class CommandHandler
+    {
+        public CommandHandler(UserRepository userRepository)
+        {
+            //
+        }
+
+        public IEnumerable<string> Handle(string command)
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
--- SocialNetwork/SocialNetwork2.Tests/AcceptanceTests.cs
+++ SocialNetwork/SocialNetwork2.Tests/AcceptanceTests.cs
@@ -62,6 +62,54 @@ namespace SocialNetwork2.Tests
             Assert.AreEqual("Alice - I love the weather today (5 minutes ago)", response2[3]);
         }
 
+        [TestMethod]
+        public void UsingTheCommandHandler()
+        {
+            var handler = new CommandHandler(userRepository);
+
+            // posting
+            Sys.Time = () => new DateTime(2000, 1, 1, 10, 0, 0);
+            handler.Handle("Alice -> I love the weather today");
+
+            Sys.Time = () => new DateTime(2000, 1, 1, 10, 3, 0);
+            handler.Handle("Bob -> Damn! We lost!");
+
+            Sys.Time = () => new DateTime(2000, 1, 1, 10, 4, 0);
+            handler.Handle("Bob -> Good game though.");
+
+            // reading
+            Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 0);
+
+            var response1 = handler.Handle("Alice").ToList();
+            Assert.AreEqual(1, response1.Count);
+            Assert.AreEqual("I love the weather today (5 minutes ago)", response1[0]);
+
+            var response2 = handler.Handle("Bob").ToList();
+            Assert.AreEqual(2, response2.Count);
+            Assert.AreEqual("Good game though. (1 minute ago)", response2[0]);
+            Assert.AreEqual("Damn! We lost! (2 minutes ago)", response2[1]);
+
+            // following
+            Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 0);
+            handler.Handle("Charlie I'm in New York today! Anyone want to have a coffee?");
+
+            Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 2);
+
+            handler.Handle("Charlie follows Alice");
+            var response3 = handler.Handle("Charlie wall").ToList();
+            Assert.AreEqual(2, response3.Count);
+            Assert.AreEqual("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)", response3[0]);
+            Assert.AreEqual("Alice - I love the weather today (5 minutes ago)", response3[1]);
+
+            handler.Handle("Charlie follows Bob");
+            var response4 = handler.Handle("Charlie wall").ToList();
+            Assert.AreEqual(4, response4.Count);
+            Assert.AreEqual("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)", response4[0]);
+            Assert.AreEqual("Bob - Good game though. (1 minute ago)", response4[1]);
+            Assert.AreEqual("Bob - Damn! We lost! (2 minutes ago)", response4[2]);
+            Assert.AreEqual("Alice - I love the weather today (5 minutes ago)", response4[3]);
+        }
+
         //
 
         private void Scenario1_Posting()
