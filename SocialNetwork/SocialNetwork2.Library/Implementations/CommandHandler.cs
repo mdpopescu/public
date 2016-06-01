254c7fc251a53264b95d3c69d260e8fce86d589a
Marcel Popescu <mdpopescu@gmail.com>
2016-06-01 08:41:55 +0300

    Refactoring

--- SocialNetwork/SocialNetwork2.Library/Implementations/CommandHandler.cs
+++ SocialNetwork/SocialNetwork2.Library/Implementations/CommandHandler.cs
@@ -10,33 +10,32 @@ namespace SocialNetwork2.Library.Implementations
             this.userRepository = userRepository;
         }
 
-        public IEnumerable<string> Handle(string command)
+        public IEnumerable<string> Handle(string input)
         {
-            var parts = command.Split(' ');
+            var parts = input.Split(' ');
 
             var userName = parts[0];
             var user = userRepository.CreateOrFind(userName);
 
-            if (parts.Length == 1)
+            var command = parts.Length == 1 ? "" : parts[1];
+            switch (command)
             {
-                return user.Read();
-            }
-            if (parts[1] == "->")
-            {
-                user.Post(string.Join(" ", parts.Skip(2)));
-                return Enumerable.Empty<string>();
-            }
-            if (parts[1] == "wall")
-            {
-                return user.Wall();
-            }
-            if (parts[1] == "follows")
-            {
-                var otherUserName = parts[2];
-                var otherUser = userRepository.CreateOrFind(otherUserName);
+                case "":
+                    return user.Read();
 
-                user.Follow(otherUser);
-                return Enumerable.Empty<string>();
+                case "->":
+                    user.Post(string.Join(" ", parts.Skip(2)));
+                    return Enumerable.Empty<string>();
+
+                case "wall":
+                    return user.Wall();
+
+                case "follows":
+                    var otherUserName = parts[2];
+                    var otherUser = userRepository.CreateOrFind(otherUserName);
+
+                    user.Follow(otherUser);
+                    return Enumerable.Empty<string>();
             }
 
             return Enumerable.Empty<string>();
