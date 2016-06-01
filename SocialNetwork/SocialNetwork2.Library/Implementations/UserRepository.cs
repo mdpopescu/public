71469c166d43d15c16e7e599d656a9a35447e334
Marcel Popescu <mdpopescu@gmail.com>
2016-06-01 06:35:11 +0300

    The user repository retrieves the same instance on subsequent calls

--- SocialNetwork/SocialNetwork2.Library/Implementations/UserRepository.cs
+++ SocialNetwork/SocialNetwork2.Library/Implementations/UserRepository.cs
@@ -1,10 +1,23 @@
-﻿namespace SocialNetwork2.Library.Implementations
+﻿using System.Collections.Generic;
+
+namespace SocialNetwork2.Library.Implementations
 {
     public class UserRepository
     {
         public User CreateOrFind(string userName)
         {
-            return null;
+            User user;
+            if (!users.TryGetValue(userName, out user))
+            {
+                user = new User(userName);
+                users.Add(userName, user);
+            }
+
+            return user;
         }
+
+        //
+
+        private readonly Dictionary<string, User> users = new Dictionary<string, User>();
     }
 }
---
+++ SocialNetwork/SocialNetwork2.Tests/Implementations/UserRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork2.Library.Implementations;
+
+namespace SocialNetwork2.Tests.Implementations
+{
+    [TestClass]
+    public class UserRepositoryTests
+    {
+        private UserRepository sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new UserRepository();
+        }
+
+        [TestMethod]
+        public void ReturnsAUserInstance()
+        {
+            var result = sut.CreateOrFind("abc");
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(User));
+        }
+
+        [TestMethod]
+        public void ReturnsTheSameInstanceOnSubsequentCalls()
+        {
+            var user1 = sut.CreateOrFind("abc");
+            var user2 = sut.CreateOrFind("abc");
+
+            Assert.AreEqual(user1, user2);
+        }
+    }
+}
