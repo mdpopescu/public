f84cdc6024bdd7aebeb2f1872484448ea23c5de2
Marcel Popescu <mdpopescu@gmail.com>
2016-06-01 06:53:25 +0300

    Refactoring - added a Duration class

---
+++ SocialNetwork/SocialNetwork2.Library/Models/Duration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SocialNetwork2.Library.Models
+{
+    public class Duration
+    {
+        public Duration(TimeSpan ts)
+        {
+            var hours = (int) Math.Truncate(ts.TotalHours);
+            var minutes = (int) Math.Truncate(ts.TotalMinutes);
+            var seconds = (int) Math.Truncate(ts.TotalSeconds);
+
+            value = hours > 0 ? hours : minutes > 0 ? minutes : seconds;
+            unit = hours > 0 ? "hour" : minutes > 0 ? "minute" : "second";
+
+            suffix = value == 1 ? "" : "s";
+        }
+
+        public override string ToString()
+        {
+            return $"{value} {unit}{suffix}";
+        }
+
+        //
+
+        private readonly int value;
+        private readonly string unit;
+        private readonly string suffix;
+    }
+}
---
+++ SocialNetwork/SocialNetwork2.Tests/Models/DurationTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork2.Library.Models;
+
+namespace SocialNetwork2.Tests.Models
+{
+    [TestClass]
+    public class DurationTests
+    {
+        [TestMethod]
+        public void ReturnsElapsedSeconds()
+        {
+            var duration = new Duration(TimeSpan.FromSeconds(2));
+
+            Assert.AreEqual("2 seconds", duration.ToString());
+        }
+
+        [TestMethod]
+        public void ReturnsElapsedSecond()
+        {
+            var duration = new Duration(TimeSpan.FromSeconds(1));
+
+            Assert.AreEqual("1 second", duration.ToString());
+        }
+
+        [TestMethod]
+        public void ReturnsElapsedMinutes()
+        {
+            var duration = new Duration(TimeSpan.FromMinutes(2));
+
+            Assert.AreEqual("2 minutes", duration.ToString());
+        }
+
+        [TestMethod]
+        public void ReturnsElapsedMinute()
+        {
+            var duration = new Duration(TimeSpan.FromMinutes(1));
+
+            Assert.AreEqual("1 minute", duration.ToString());
+        }
+
+        [TestMethod]
+        public void ReturnsElapsedHours()
+        {
+            var duration = new Duration(TimeSpan.FromHours(2));
+
+            Assert.AreEqual("2 hours", duration.ToString());
+        }
+
+        [TestMethod]
+        public void ReturnsElapsedHour()
+        {
+            var duration = new Duration(TimeSpan.FromHours(1));
+
+            Assert.AreEqual("1 hour", duration.ToString());
+        }
+    }
+}
