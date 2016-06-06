9c79118d9726117a673a379385cd3b3e9962eac5
Marcel Popescu <mdpopescu@gmail.com>
2016-06-06 08:23:29 +0300

    The user-specific keys are saved locally in an encrypted text file

--- TweetNicer/TweetNicer.Spike/Program.cs
+++ TweetNicer/TweetNicer.Spike/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Reactive.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using CoreTweet;
 using CoreTweet.Streaming;
@@ -14,13 +17,15 @@ namespace TweetNicer.Spike
         {
             var settings = ConfigurationManager.AppSettings;
 
-            var tokens = new Tokens
-            {
-                ConsumerKey = settings["ConsumerKey"],
-                ConsumerSecret = settings["ConsumerSecret"],
-                AccessToken = settings["AccessToken"],
-                AccessTokenSecret = settings["AccessTokenSecret"],
-            };
+            var tokens = GetTokens(settings["ConsumerKey"], settings["ConsumerSecret"]);
+
+            //var tokens = new Tokens
+            //{
+            //    ConsumerKey = settings["ConsumerKey"],
+            //    ConsumerSecret = settings["ConsumerSecret"],
+            //    AccessToken = settings["AccessToken"],
+            //    AccessTokenSecret = settings["AccessTokenSecret"],
+            //};
 
             //var session = OAuth.Authorize(settings["ConsumerKey"], settings["ConsumerSecret"]);
             //Process.Start(session.AuthorizeUri.AbsoluteUri);
@@ -28,14 +33,68 @@ namespace TweetNicer.Spike
             //var pin = Console.ReadLine();
             //var tokens = session.GetTokens(pin);
 
-            using (tokens
-                .Streaming
-                .FilterAsObservable(track => "football")
-                .OfType<StatusMessage>()
-                .Subscribe(msg => Console.WriteLine(Align(msg.Status.User.ScreenName + ": ", msg.Status.Text))))
-            {
+            var stream = tokens.Streaming.UserAsObservable().Publish();
+
+            //stream.OfType<FriendsMessage>()
+            //    .Subscribe(x => Console.WriteLine("Following: " + string.Join(", ", x)));
+
+            stream.OfType<StatusMessage>()
+                .Subscribe(x => Console.WriteLine(Align(x.Status.User.ScreenName + ": ", x.Status.Text)));
+
+            using (stream.Connect())
                 Console.ReadLine();
+
+            //var stream = tokens
+            //    .Streaming
+            //    .FilterAsObservable(track => "football")
+            //    .OfType<StatusMessage>();
+
+            //using (stream.Subscribe(msg => Console.WriteLine(Align(msg.Status.User.ScreenName + ": ", msg.Status.Text))))
+            //{
+            //    Console.ReadLine();
+            //}
+        }
+
+        private static Tokens GetTokens(string consumerKey, string consumerSecret)
+        {
+            try
+            {
+                var userSecret = File.ReadAllLines("user.secret").Select(Decrypt).ToArray();
+                return new Tokens
+                {
+                    ConsumerKey = consumerKey,
+                    ConsumerSecret = consumerSecret,
+                    AccessToken = userSecret[0],
+                    AccessTokenSecret = userSecret[1],
+                };
             }
+            catch (Exception)
+            {
+                var session = OAuth.Authorize(consumerKey, consumerSecret);
+                Process.Start(session.AuthorizeUri.AbsoluteUri);
+                Console.Write("Enter PIN: ");
+                var pin = Console.ReadLine();
+                var tokens = session.GetTokens(pin);
+
+                var userSecret = new[] { tokens.AccessToken, tokens.AccessTokenSecret };
+                File.WriteAllLines("user.secret", userSecret.Select(Encrypt));
+
+                return tokens;
+            }
+        }
+
+        private static string Decrypt(string text)
+        {
+            var encrypted = Convert.FromBase64String(text);
+            var plaintext = ProtectedData.Unprotect(encrypted, null, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(plaintext);
+        }
+
+        private static string Encrypt(string text)
+        {
+            var plaintext = Encoding.UTF8.GetBytes(text);
+            var encrypted = ProtectedData.Protect(plaintext, null, DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(encrypted);
         }
 
         private static string Align(string prefix, string text, int length = 80)
