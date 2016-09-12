8c64bf4149dd534200f4c3e9a35611a7fae64d29
Marcel Popescu <mdpopescu@gmail.com>
2016-09-12 09:41:16 +0300

    Added a tester program and one command implementation which returns a hardcoded version number

---
+++ Elomen/Elomen.Commands/Implementations/VersionCommand.cs
@@ -0,0 +1,13 @@
+﻿using Elomen.Library.Contracts;
+using Elomen.Library.Model;
+
+namespace Elomen.Commands.Implementations
+{
+    public class VersionCommand : Command
+    {
+        public string Execute(Account account)
+        {
+            return "Version 0.01";
+        }
+    }
+}
---
+++ Elomen/Elomen.Tester/Implementations/NullAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Elomen.Library.Contracts;
+using Elomen.Library.Model;
+
+namespace Elomen.Tester.Implementations
+{
+    public class NullAccountRepository : AccountRepository
+    {
+        public Account Find(string accountId)
+        {
+            return Account.GUEST;
+        }
+    }
+}
