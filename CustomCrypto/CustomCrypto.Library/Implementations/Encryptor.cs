64f8a9359ba4057b2a12341c874917afe1d38b4d
Marcel Popescu <mdpopescu@gmail.com>
2016-12-18 11:43:41 +0200

    Encryption / decryption works; also added benchmarks

---
+++ CustomCrypto/CustomCrypto.Library/Implementations/Encryptor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CustomCrypto.Library.Contracts;
+
+namespace CustomCrypto.Library.Implementations
+{
+    public class Encryptor
+    {
+        public Encryptor(string key)
+        {
+            keyRNG = new PredictableRNG(key.GetHashCode());
+            outputRNG = new SecureRNG();
+        }
+
+        public uint[] Encrypt(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var bits = new BitArray(bytes);
+
+            return bits
+                .Cast<bool>()
+                .Select(EncryptBit)
+                .ToArray();
+        }
+
+        public string Decrypt(uint[] encrypted)
+        {
+            var bits = new BitArray(encrypted.Select(DecryptBit).ToArray());
+            var bytes = BitArrayToByteArray(bits).ToArray();
+
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        //
+
+        private readonly RNG keyRNG;
+        private readonly RNG outputRNG;
+
+        private uint EncryptBit(bool bit)
+        {
+            var output = outputRNG.Next();
+
+            var index = (int) (keyRNG.Next() % 32);
+            var mask = (uint) 1 << index;
+
+            output &= ~mask;
+            if (bit)
+                output |= mask;
+
+            return output;
+        }
+
+        private bool DecryptBit(uint encrypted)
+        {
+            var index = (int) (keyRNG.Next() % 32);
+            var mask = (uint) 1 << index;
+
+            return (encrypted & mask) != 0;
+        }
+
+        private static IEnumerable<byte> BitArrayToByteArray(BitArray bits)
+        {
+            var ret = new byte[(bits.Length - 1) / 8 + 1];
+            bits.CopyTo(ret, 0);
+
+            return ret;
+        }
+    }
+}
--- CustomCrypto/CustomCrypto.Tester/Program.cs
+++ CustomCrypto/CustomCrypto.Tester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using CustomCrypto.Library.Implementations;
 
 namespace CustomCrypto.Tester
 {
@@ -6,7 +8,76 @@ namespace CustomCrypto.Tester
     {
         private static void Main(string[] args)
         {
-            var rnd = new Random();
+            //Test();
+            BenchmarkEncrypt();
+            BenchmarkDecrypt();
+        }
+
+        //
+
+        private const string KEY = "some key";
+
+        private const int COUNT = 100000;
+
+        private static void Test()
+        {
+            do
+            {
+                Console.Write("Enter the message to encrypt:");
+                var message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message))
+                    break;
+
+                var encrypted = Encrypt(message);
+                Console.WriteLine($"{encrypted.Length} words:");
+                foreach (var it in encrypted)
+                    Console.Write("{0:X8}  ", it);
+                Console.WriteLine();
+
+                var decrypted = Decrypt(encrypted);
+
+                Console.WriteLine($"The decrypted message is [{decrypted}]");
+            } while (true);
+        }
+
+        private static void BenchmarkEncrypt()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (var i = 1; i <= COUNT; i++)
+                Encrypt("some message");
+
+            sw.Stop();
+
+            Console.WriteLine($"Time spent to encrypt a message {COUNT} times: {sw.Elapsed}");
+        }
+
+        private static void BenchmarkDecrypt()
+        {
+            var encrypted = Encrypt("some message");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (var i = 1; i <= COUNT; i++)
+                Decrypt(encrypted);
+
+            sw.Stop();
+
+            Console.WriteLine($"Time spent to decrypt a message {COUNT} times: {sw.Elapsed}");
+        }
+
+        private static uint[] Encrypt(string message)
+        {
+            var encryptor = new Encryptor(KEY);
+            return encryptor.Encrypt(message);
+        }
+
+        private static string Decrypt(uint[] encrypted)
+        {
+            var encryptor = new Encryptor(KEY);
+            return encryptor.Decrypt(encrypted);
         }
     }
 }
