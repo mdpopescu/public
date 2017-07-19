630cbe96cbfca973b9cce9a36c9e95fdd52a5291
Marcel Popescu <mdpopescu@gmail.com>
2017-07-19 17:00:22 +0300

    Initial commit

---
+++ Acta2/Acta2.Tests/AcceptanceTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acta2.Tests
+{
+    [TestClass]
+    public class AcceptanceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
