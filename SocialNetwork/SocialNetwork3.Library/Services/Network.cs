af858eb9155ce34186c686332c634120bf93a807
Marcel Popescu <mdpopescu@gmail.com>
2017-09-04 08:36:42 +0300

    Added the end-to-end acceptance test

--- SocialNetwork/SocialNetwork3.Library/Services/Network.cs
+++ SocialNetwork/SocialNetwork3.Library/Services/Network.cs
@@ -3,8 +3,13 @@ using System.Linq;
 
 namespace SocialNetwork3.Library.Services
 {
-    public class NetworkCoordinator
+    public class Network
     {
+        /// <summary>
+        /// Enters the specified line.
+        /// </summary>
+        /// <param name="line">The line.</param>
+        /// <returns>The response lines, if any.</returns>
         public IEnumerable<string> Enter(string line)
         {
             return Enumerable.Empty<string>();
--- SocialNetwork/SocialNetwork3.Tests/AcceptanceTests.cs
+++ SocialNetwork/SocialNetwork3.Tests/AcceptanceTests.cs
@@ -11,29 +11,29 @@ namespace SocialNetwork3.Tests
         [TestMethod]
         public void EndToEnd()
         {
-            var input = new NetworkCoordinator();
+            var network = new Network();
 
             // posting
             Sys.Time = () => new DateTime(2000, 1, 1, 10, 0, 0);
-            input.Enter("Alice -> I love the weather today");
+            network.Enter("Alice -> I love the weather today");
 
             Sys.Time = () => new DateTime(2000, 1, 1, 10, 3, 0);
-            input.Enter("Bob -> Damn! We lost!");
+            network.Enter("Bob -> Damn! We lost!");
 
             Sys.Time = () => new DateTime(2000, 1, 1, 10, 4, 0);
-            input.Enter("Bob -> Good game though.");
+            network.Enter("Bob -> Good game though.");
 
             Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 0);
-            input.Enter("Charlie -> I'm in New York today! Anyone want to have a coffee?");
+            network.Enter("Charlie -> I'm in New York today! Anyone want to have a coffee?");
 
             // reading
             Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 0);
 
-            var response1 = input.Enter("Alice").ToList();
+            var response1 = network.Enter("Alice").ToList();
             Assert.AreEqual(1, response1.Count);
             Assert.AreEqual("I love the weather today (5 minutes ago)", response1[0]);
 
-            var response2 = input.Enter("Bob").ToList();
+            var response2 = network.Enter("Bob").ToList();
             Assert.AreEqual(2, response2.Count);
             Assert.AreEqual("Good game though. (1 minute ago)", response2[0]);
             Assert.AreEqual("Damn! We lost! (2 minutes ago)", response2[1]);
@@ -41,14 +41,14 @@ namespace SocialNetwork3.Tests
             // following
             Sys.Time = () => new DateTime(2000, 1, 1, 10, 5, 2);
 
-            input.Enter("Charlie follows Alice");
-            var response3 = input.Enter("Charlie wall").ToList();
+            network.Enter("Charlie follows Alice");
+            var response3 = network.Enter("Charlie wall").ToList();
             Assert.AreEqual(2, response3.Count);
             Assert.AreEqual("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)", response3[0]);
             Assert.AreEqual("Alice - I love the weather today (5 minutes ago)", response3[1]);
 
-            input.Enter("Charlie follows Bob");
-            var response4 = input.Enter("Charlie wall").ToList();
+            network.Enter("Charlie follows Bob");
+            var response4 = network.Enter("Charlie wall").ToList();
             Assert.AreEqual(4, response4.Count);
             Assert.AreEqual("Charlie - I'm in New York today! Anyone want to have a coffee? (2 seconds ago)", response4[0]);
             Assert.AreEqual("Bob - Good game though. (1 minute ago)", response4[1]);
