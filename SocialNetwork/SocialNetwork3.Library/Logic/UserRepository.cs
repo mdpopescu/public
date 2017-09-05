06407d4ff294ecb6116f49d2e6634e15364e5d2f
Marcel Popescu <mdpopescu@gmail.com>
2017-09-05 06:11:52 +0300

    The user repository works correctly

--- SocialNetwork/SocialNetwork3.Library/Logic/UserRepository.cs
+++ SocialNetwork/SocialNetwork3.Library/Logic/UserRepository.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SocialNetwork3.Library.Logic
 {
     public class UserRepository
     {
+        /// <summary>Adds a follower to the given user.</summary>
+        /// <param name="follower">The follower.</param>
+        /// <param name="followed">The user being followed.</param>
         public void AddFollower(string follower, string followed)
         {
-            //
+            // does not allow a user to follow himself
+            if (string.Equals(follower, followed, StringComparison.OrdinalIgnoreCase))
+                return;
+
+            if (!followers.ContainsKey(followed))
+                followers.Add(followed, new List<string>());
+
+            var list = followers[followed];
+            if (!list.Contains(follower, StringComparer.OrdinalIgnoreCase))
+                list.Add(follower);
         }
 
-        public IEnumerable<string> GetFollowers(string user)
-        {
-            return Enumerable.Empty<string>();
-        }
+        /// <summary>Gets the followers of the given user.</summary>
+        /// <param name="user">The user.</param>
+        /// <returns>The list of followers.</returns>
+        public IEnumerable<string> GetFollowers(string user) => followers.ContainsKey(user) ? followers[user] : Enumerable.Empty<string>();
+
+        //
+
+        private readonly Dictionary<string, List<string>> followers = new Dictionary<string, List<string>>();
     }
 }
---
+++ SocialNetwork/SocialNetwork3.Tests/Logic/UserRepositoryTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork3.Library.Logic;
+
+namespace SocialNetwork3.Tests.Logic
+{
+    [TestClass]
+    public class UserRepositoryTests
+    {
+        private UserRepository sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new UserRepository();
+        }
+
+        [TestMethod]
+        public void ReturnsTheFollowers()
+        {
+            sut.AddFollower("a", "z");
+            sut.AddFollower("b", "z");
+
+            var result = sut.GetFollowers("z").ToArray();
+
+            CollectionAssert.AreEqual(new[] { "a", "b" }, result);
+        }
+
+        [TestMethod]
+        public void DoesNotAddTheSameFollowerTwice()
+        {
+            sut.AddFollower("a", "z");
+            sut.AddFollower("b", "z");
+            sut.AddFollower("a", "z");
+
+            var result = sut.GetFollowers("z").ToArray();
+
+            CollectionAssert.AreEqual(new[] { "a", "b" }, result);
+        }
+
+        [TestMethod]
+        public void DoesNotAddAUserAsFollowerToHimself()
+        {
+            sut.AddFollower("a", "z");
+            sut.AddFollower("b", "z");
+            sut.AddFollower("z", "z");
+
+            var result = sut.GetFollowers("z").ToArray();
+
+            CollectionAssert.AreEqual(new[] { "a", "b" }, result);
+        }
+    }
+}
