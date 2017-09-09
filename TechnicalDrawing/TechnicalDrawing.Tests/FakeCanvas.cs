d75ab80052e24420c68ec8d2069a5bb803dd9725
Marcel Popescu <mdpopescu@gmail.com>
2017-09-09 04:32:30 +0300

    The acceptance test passes

---
+++ TechnicalDrawing/TechnicalDrawing.Tests/FakeCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TechnicalDrawing.Library.Contracts;
+using TechnicalDrawing.Library.Models;
+
+namespace TechnicalDrawing.Tests
+{
+    public class FakeCanvas : Canvas
+    {
+        public List<string> Commands { get; } = new List<string>();
+
+        public void Execute(ProjectedCommand command) => Commands.Add(command.ToString());
+    }
+}
--- TechnicalDrawing/TechnicalDrawing.Tests/Shell/AcceptanceTests.cs
+++ TechnicalDrawing/TechnicalDrawing.Tests/Shell/AcceptanceTests.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using TechnicalDrawing.Library.Contracts;
 using TechnicalDrawing.Library.Core;
 using TechnicalDrawing.Library.Models;
 using TechnicalDrawing.Library.Shell;
@@ -13,17 +11,18 @@ namespace TechnicalDrawing.Tests.Shell
         [TestMethod]
         public void Test1()
         {
-            var canvas = new Mock<Canvas>();
-            var app = new DrawingApp(new WinFileSystem(), new Parser(), new Projector(), canvas.Object);
+            var canvas = new FakeCanvas();
+            var app = new DrawingApp(new WinFileSystem(), new Parser(), new Projector(), canvas);
 
             app.Load(@"Data\d1.txt");
 
-            canvas.Verify(it => it.Execute(new ProjectedCommand(Plane.XY, CommandName.Line, new QuadrantPoint(10, 20), new QuadrantPoint(40, 50))));
-            canvas.Verify(it => it.Execute(new ProjectedCommand(Plane.XY, CommandName.Line, new QuadrantPoint(20, 30), new QuadrantPoint(50, 60))));
-            canvas.Verify(it => it.Execute(new ProjectedCommand(Plane.XZ, CommandName.Line, new QuadrantPoint(10, 30), new QuadrantPoint(40, 60))));
-            canvas.Verify(it => it.Execute(new ProjectedCommand(Plane.XZ, CommandName.Line, new QuadrantPoint(20, 40), new QuadrantPoint(50, 70))));
-            canvas.Verify(it => it.Execute(new ProjectedCommand(Plane.YZ, CommandName.Line, new QuadrantPoint(20, 30), new QuadrantPoint(50, 60))));
-            canvas.Verify(it => it.Execute(new ProjectedCommand(Plane.YZ, CommandName.Line, new QuadrantPoint(30, 40), new QuadrantPoint(60, 70))));
+            Assert.AreEqual(6, canvas.Commands.Count);
+            Assert.AreEqual(new ProjectedCommand(Plane.XY, CommandName.Line, new QuadrantPoint(10, 20), new QuadrantPoint(40, 50)).ToString(), canvas.Commands[0]);
+            Assert.AreEqual(new ProjectedCommand(Plane.XY, CommandName.Line, new QuadrantPoint(20, 30), new QuadrantPoint(50, 60)).ToString(), canvas.Commands[1]);
+            Assert.AreEqual(new ProjectedCommand(Plane.XZ, CommandName.Line, new QuadrantPoint(10, 30), new QuadrantPoint(40, 60)).ToString(), canvas.Commands[2]);
+            Assert.AreEqual(new ProjectedCommand(Plane.XZ, CommandName.Line, new QuadrantPoint(20, 40), new QuadrantPoint(50, 70)).ToString(), canvas.Commands[3]);
+            Assert.AreEqual(new ProjectedCommand(Plane.YZ, CommandName.Line, new QuadrantPoint(20, 30), new QuadrantPoint(50, 60)).ToString(), canvas.Commands[4]);
+            Assert.AreEqual(new ProjectedCommand(Plane.YZ, CommandName.Line, new QuadrantPoint(30, 40), new QuadrantPoint(60, 70)).ToString(), canvas.Commands[5]);
         }
     }
 }
