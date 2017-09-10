9b05e7e0e45fd38c0b4420e67f5703eab5a0ac4a
Marcel Popescu <mdpopescu@gmail.com>
2017-09-10 05:20:57 +0300

    Refactoring

--- TechnicalDrawing/TechnicalDrawing.Tests/Shell/AcceptanceTests.cs
+++ TechnicalDrawing/TechnicalDrawing.Tests/Shell/AcceptanceTests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TechnicalDrawing.Library.Core;
-using TechnicalDrawing.Library.Models;
 using TechnicalDrawing.Library.Shell;
 
 namespace TechnicalDrawing.Tests.Shell
@@ -28,18 +27,18 @@ namespace TechnicalDrawing.Tests.Shell
             Assert.AreEqual(3, canvas.Commands.Count);
             CollectionAssert.AreEqual(new[]
             {
-                new ProjectedCommand(Plane.XY, CommandName.Line, new Point2D(10, 20), new Point2D(40, 50)).ToString(),
-                new ProjectedCommand(Plane.XY, CommandName.Line, new Point2D(20, 30), new Point2D(50, 60)).ToString(),
+                "XY Line (10, 20), (40, 50)",
+                "XY Line (20, 30), (50, 60)",
             }, canvas.Commands[0]);
             CollectionAssert.AreEqual(new[]
             {
-                new ProjectedCommand(Plane.XZ, CommandName.Line, new Point2D(10, 30), new Point2D(40, 60)).ToString(),
-                new ProjectedCommand(Plane.XZ, CommandName.Line, new Point2D(20, 40), new Point2D(50, 70)).ToString(),
+                "XZ Line (10, 30), (40, 60)",
+                "XZ Line (20, 40), (50, 70)",
             }, canvas.Commands[1]);
             CollectionAssert.AreEqual(new[]
             {
-                new ProjectedCommand(Plane.YZ, CommandName.Line, new Point2D(20, 30), new Point2D(50, 60)).ToString(),
-                new ProjectedCommand(Plane.YZ, CommandName.Line, new Point2D(30, 40), new Point2D(60, 70)).ToString(),
+                "YZ Line (20, 30), (50, 60)",
+                "YZ Line (30, 40), (60, 70)",
             }, canvas.Commands[2]);
         }
 
@@ -51,18 +50,18 @@ namespace TechnicalDrawing.Tests.Shell
             Assert.AreEqual(3, canvas.Commands.Count);
             CollectionAssert.AreEqual(new[]
             {
-                new ProjectedCommand(Plane.XY, CommandName.Line, new Point2D(10, 20), new Point2D(40, 50)).ToString(),
-                new ProjectedCommand(Plane.XY, CommandName.Line, new Point2D(20, 30), new Point2D(50, 60)).ToString(),
+                "XY Line (10, 20), (40, 50)",
+                "XY Line (20, 30), (50, 60)",
             }, canvas.Commands[0]);
             CollectionAssert.AreEqual(new[]
             {
-                new ProjectedCommand(Plane.XZ, CommandName.Line, new Point2D(10, 30), new Point2D(40, 60)).ToString(),
-                new ProjectedCommand(Plane.XZ, CommandName.Line, new Point2D(20, 40), new Point2D(50, 70)).ToString(),
+                "XZ Line (10, 30), (40, 60)",
+                "XZ Line (20, 40), (50, 70)",
             }, canvas.Commands[1]);
             CollectionAssert.AreEqual(new[]
             {
-                new ProjectedCommand(Plane.YZ, CommandName.Line, new Point2D(20, 30), new Point2D(50, 60)).ToString(),
-                new ProjectedCommand(Plane.YZ, CommandName.Line, new Point2D(30, 40), new Point2D(60, 70)).ToString(),
+                "YZ Line (20, 30), (50, 60)",
+                "YZ Line (30, 40), (60, 70)",
             }, canvas.Commands[2]);
         }
     }
