c6148286f8f57742ba09f9115de9a042dfc8650d
Marcel Popescu <mdpopescu@gmail.com>
2017-09-10 14:23:43 +0300

    The Pomodoro program works

--- Pomodoro/Pomodoro/MainForm.Designer.cs
+++ Pomodoro/Pomodoro/MainForm.Designer.cs
@@ -28,13 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.rb10min = new System.Windows.Forms.RadioButton();
+            this.lblRemaining = new System.Windows.Forms.Label();
             this.rb20min = new System.Windows.Forms.RadioButton();
-            this.rb30min = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
+            this.rb30min = new System.Windows.Forms.RadioButton();
+            this.rb10min = new System.Windows.Forms.RadioButton();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuRestore = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
+            this.contextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -43,7 +51,7 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblRemaining, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.rb20min, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.btnStart, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.rb30min, 1, 1);
@@ -58,16 +66,17 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(445, 145);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
-            // rb10min
+            // lblRemaining
             // 
-            this.rb10min.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.rb10min.AutoSize = true;
-            this.rb10min.Location = new System.Drawing.Point(30, 107);
-            this.rb10min.Name = "rb10min";
-            this.rb10min.Size = new System.Drawing.Size(87, 27);
-            this.rb10min.TabIndex = 2;
-            this.rb10min.Text = "10 min";
-            this.rb10min.UseVisualStyleBackColor = true;
+            this.lblRemaining.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblRemaining.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.lblRemaining, 2);
+            this.lblRemaining.Font = new System.Drawing.Font("Georgia", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRemaining.Location = new System.Drawing.Point(45, 12);
+            this.lblRemaining.Name = "lblRemaining";
+            this.lblRemaining.Size = new System.Drawing.Size(206, 72);
+            this.lblRemaining.TabIndex = 5;
+            this.lblRemaining.Text = "00:00";
             // 
             // rb20min
             // 
@@ -82,6 +91,17 @@
             this.rb20min.Text = "20 min";
             this.rb20min.UseVisualStyleBackColor = true;
             // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStart.Location = new System.Drawing.Point(320, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 48);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
             // rb30min
             // 
             this.rb30min.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -93,27 +113,50 @@
             this.rb30min.Text = "30 min";
             this.rb30min.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // rb10min
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
-            this.label1.Font = new System.Drawing.Font("Georgia", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(45, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 72);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "00:00";
+            this.rb10min.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rb10min.AutoSize = true;
+            this.rb10min.Location = new System.Drawing.Point(30, 107);
+            this.rb10min.Name = "rb10min";
+            this.rb10min.Size = new System.Drawing.Size(87, 27);
+            this.rb10min.TabIndex = 2;
+            this.rb10min.Text = "10 min";
+            this.rb10min.UseVisualStyleBackColor = true;
             // 
-            // btnStart
+            // notifyIcon
             // 
-            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnStart.Location = new System.Drawing.Point(320, 24);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(101, 48);
-            this.btnStart.TabIndex = 6;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.notifyIcon.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Pomodoro";
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuRestore,
+            this.mnuExit});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenu.Size = new System.Drawing.Size(114, 48);
+            // 
+            // mnuRestore
+            // 
+            this.mnuRestore.Name = "mnuRestore";
+            this.mnuRestore.Size = new System.Drawing.Size(113, 22);
+            this.mnuRestore.Text = "Restore";
+            this.mnuRestore.Click += new System.EventHandler(this.mnuRestore_Click);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(113, 22);
+            this.mnuExit.Text = "Exit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // MainForm
             // 
@@ -122,11 +165,15 @@
             this.ClientSize = new System.Drawing.Size(445, 145);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "MainForm";
             this.Text = "Pomodoro";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.contextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -134,11 +181,16 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRemaining;
         private System.Windows.Forms.RadioButton rb30min;
         private System.Windows.Forms.RadioButton rb20min;
         private System.Windows.Forms.RadioButton rb10min;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem mnuRestore;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- Pomodoro/Pomodoro/MainForm.cs
+++ Pomodoro/Pomodoro/MainForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Pomodoro
@@ -16,5 +11,115 @@ namespace Pomodoro
         {
             InitializeComponent();
         }
+
+        //
+
+        private TimeSpan remaining;
+
+        private bool IsRunning => timer.Enabled;
+
+        private TimeSpan SelectedDuration =>
+            rb10min.Checked
+                ? TimeSpan.FromMinutes(10)
+                : rb20min.Checked
+                    ? TimeSpan.FromMinutes(20)
+                    : TimeSpan.FromMinutes(30);
+
+        private void Minimize()
+        {
+            notifyIcon.Visible = true;
+            Hide();
+        }
+
+        private void Restore()
+        {
+            notifyIcon.Visible = false;
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Start()
+        {
+            remaining = SelectedDuration;
+
+            rb10min.Enabled = rb20min.Enabled = rb30min.Enabled = false;
+
+            btnStart.Text = "Stop";
+            btnStart.ForeColor = Color.Red;
+            timer.Enabled = true;
+        }
+
+        private void Stop()
+        {
+            if (WindowState == FormWindowState.Minimized)
+                Restore();
+
+            rb10min.Enabled = rb20min.Enabled = rb30min.Enabled = true;
+
+            timer.Enabled = false;
+            btnStart.Text = "Start";
+            btnStart.ForeColor = SystemColors.ControlText;
+
+            lblRemaining.Text = "00:00";
+        }
+
+        private static void SoundAlarm()
+        {
+            using (var player = new SoundPlayer())
+            {
+                player.SoundLocation = @"Media\ring.wav";
+                player.Play();
+            }
+        }
+
+        //
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+                Minimize();
+            else
+                Restore();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lblRemaining.Text = remaining.ToString(@"mm\:ss");
+            notifyIcon.Text = remaining.ToString(@"mm\:ss");
+
+            remaining = remaining.Subtract(TimeSpan.FromSeconds(1));
+            if (remaining > TimeSpan.Zero)
+                return;
+
+            SoundAlarm();
+            Stop();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "Start")
+                Start();
+            else
+                Stop();
+        }
+
+        private void mnuRestore_Click(object sender, EventArgs e)
+        {
+            Restore();
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            if (IsRunning && MessageBox.Show("Are you sure you want to exit?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
     }
 }
+
+// Using image from https://pixabay.com/en/alarm-clock-clock-time-wake-up-155187/
+// Using sound file from http://www.orangefreesounds.com/short-ringtone-for-notification/
