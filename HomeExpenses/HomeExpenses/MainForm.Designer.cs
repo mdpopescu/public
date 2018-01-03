fa091b3fb514c01f041f1470b9bf6d74bfaeaf12
Marcel Popescu <mdpopescu@gmail.com>
2018-01-03 07:54:15 +0200

    Added the HomeExpenses project

---
+++ HomeExpenses/HomeExpenses/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HomeExpenses
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.rgvExpenses = new Telerik.WinControls.UI.RadGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.radPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rgvExpenses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rgvExpenses.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.Controls.Add(this.rgvExpenses);
+            this.radPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radPanel1.Location = new System.Drawing.Point(0, 97);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(1170, 394);
+            this.radPanel1.TabIndex = 7;
+            // 
+            // rgvExpenses
+            // 
+            this.rgvExpenses.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rgvExpenses.Location = new System.Drawing.Point(0, 0);
+            // 
+            // 
+            // 
+            this.rgvExpenses.MasterTemplate.ViewDefinition = tableViewDefinition1;
+            this.rgvExpenses.Name = "rgvExpenses";
+            this.rgvExpenses.Size = new System.Drawing.Size(1170, 394);
+            this.rgvExpenses.TabIndex = 0;
+            this.rgvExpenses.Text = "radGridView1";
+            this.rgvExpenses.RowsChanged += new Telerik.WinControls.UI.GridViewCollectionChangedEventHandler(this.rgvExpenses_RowsChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 491);
+            this.Controls.Add(this.radPanel1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainForm";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "Home Expenses";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.radPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rgvExpenses.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rgvExpenses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private Telerik.WinControls.UI.RadGridView rgvExpenses;
+    }
+}
+
---
+++ HomeExpenses/HomeExpenses/MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Drawing;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace HomeExpenses
+{
+    public partial class MainForm : RadForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //
+
+        private readonly HomeEntities db = new HomeEntities();
+
+        private void SetupGrid()
+        {
+            rgvExpenses.AutoGenerateColumns = false;
+
+            AddDateColumn("Date", "Data", 100);
+            AddTextColumn("Amount", "Suma", 80);
+            AddTextColumn("Description", "Scopul cheltuielii", 400);
+            AddComboColumn("Category", "Categoria", 150, db.Categories.Local.ToBindingList());
+        }
+
+        private void AddDateColumn(string name, string header, int width)
+        {
+            rgvExpenses.Columns.Add(new GridViewDateTimeColumn(name) { HeaderText = header, TextAlignment = ContentAlignment.MiddleCenter, MinWidth = width });
+        }
+
+        private void AddTextColumn(string name, string header, int width)
+        {
+            rgvExpenses.Columns.Add(new GridViewTextBoxColumn(name) { HeaderText = header, TextAlignment = ContentAlignment.MiddleRight, MinWidth = width });
+        }
+
+        private void AddComboColumn(string name, string header, int width, BindingList<Categories> values)
+        {
+            rgvExpenses.Columns.Add(
+                new GridViewComboBoxColumn(name)
+                {
+                    HeaderText = header,
+                    TextAlignment = ContentAlignment.MiddleCenter,
+                    MinWidth = width,
+                    DropDownStyle = RadDropDownStyle.DropDown,
+                    DataSource = values,
+                    DisplayMember = "Category",
+                    ValueMember = "Category",
+                });
+        }
+
+        //
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetupGrid();
+            rgvExpenses.DataSource = db.Expenses.Local.ToBindingList();
+        }
+
+        private void rgvExpenses_RowsChanged(object sender, GridViewCollectionChangedEventArgs e)
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                RadMessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
