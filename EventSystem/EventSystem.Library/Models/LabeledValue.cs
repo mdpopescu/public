7bae63f7e8d6398476a5033599b96a1430db4431
Marcel Popescu <mdpopescu@gmail.com>
2018-03-09 04:11:57 +0200

    Refactoring

---
+++ EventSystem/EventSystem.Library/Models/LabeledValue.cs
@@ -0,0 +1,14 @@
+﻿namespace EventSystem.Library.Models
+{
+    public class LabeledValue
+    {
+        public string Label { get; }
+        public object Value { get; }
+
+        public LabeledValue(string label, object value)
+        {
+            Label = label;
+            Value = value;
+        }
+    }
+}
--- EventSystem/Tester/Program.cs
+++ EventSystem/Tester/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reactive.Linq;
 using EventSystem.Library.Implementations;
+using EventSystem.Library.Models;
 using Tester.Models;
 
 namespace Tester
@@ -14,16 +15,16 @@ namespace Tester
             Console.Clear();
             Console.CursorVisible = false;
 
-            EventBus.AddSource(Observable.Interval(TimeSpan.FromMilliseconds(5)).Select(_ => new Tick()));
-            EventBus.AddTransformation<Tick, Notification>(GenerateNotification);
-            EventBus.AddSink<Notification>(DisplayNotification);
+            EventBus.AddSource(Observable.Interval(TimeSpan.FromMilliseconds(5)).Select(_ => new LabeledValue("tick", null)));
+            EventBus.AddTransformation("tick", _ => new LabeledValue("notification", GenerateNotification()));
+            EventBus.AddSink("notification", it => DisplayNotification((Notification) it.Value));
 
             Console.ReadLine();
 
             Console.CursorVisible = true;
         }
 
-        private static Notification GenerateNotification(Tick _)
+        private static Notification GenerateNotification()
         {
             return new Notification(RNG.Next(22), RNG.Next(70), "*")
             {
