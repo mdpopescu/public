62f0ea965304e6eabec4be3e1978c51b80b638ec
Marcel Popescu <mdpopescu@gmail.com>
2018-04-22 12:36:45 +0300

    Added the Challenge4 project - based on http://programmingwithoutifs.blogspot.ro/2009/03/challenge-four-doktats-elevator.html

---
+++ NoIfsChallenges/Challenge4/Challenge4/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Challenge4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //
+
+        //
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnCallTo3rd_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnGoFrom3rdTo1st_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnGoFrom3rdTo2nd_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnCallTo2nd_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnGoUp_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnGoDown_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnCallTo1st_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnGoFrom1stTo3rd_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnGoFrom1stTo2nd_Click(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
