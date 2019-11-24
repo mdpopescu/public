9abca4094d3c55d803ea6e93cd4ef282cd35bf41
Marcel Popescu <mdpopescu@gmail.com>
2019-11-24 02:53:35 +0200

    Refactoring

--- TransportTycoon/TransportTycoon.Library/Services/Map.cs
+++ TransportTycoon/TransportTycoon.Library/Services/Map.cs
@@ -16,45 +16,10 @@ namespace TransportTycoon.Library.Services
 
         public IEnumerable<Link> GetRoute(Endpoint origin, Endpoint destination)
         {
-            // Dijkstra's shortest path algorithm -- based on https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
-
             var nodes = GetNodes().ToArray();
-
-            var q = new List<Endpoint>();
-            var dist = new Dictionary<Endpoint, int>();
-            var prev = new Dictionary<Endpoint, Endpoint?>();
-
-            foreach (var v in nodes)
-            {
-                dist[v] = int.MaxValue;
-                prev[v] = null;
-                q.Add(v);
-            }
-
-            dist[origin] = 0;
-
-            while (q.Any())
-            {
-                var u = q.OrderBy(it => dist[it]).First();
-                q.Remove(u);
-
-                if (u == destination)
-                    return FindLinks(BuildPath(prev, origin, destination).ToArray());
-
-                foreach (var link in GetLinks(u, q))
-                {
-                    var v = link.GetOther(u);
-
-                    var alt = dist[u] + link.Cost;
-                    if (alt >= dist[v])
-                        continue;
-
-                    dist[v] = alt;
-                    prev[v] = u;
-                }
-            }
-
-            return Enumerable.Empty<Link>();
+            var solution = Dijkstra(nodes, origin, destination);
+            var path = BuildPath(solution, origin, destination).ToArray();
+            return FindLinks(path);
         }
 
         //
@@ -74,6 +39,65 @@ namespace TransportTycoon.Library.Services
             return result;
         }
 
+        private IReadOnlyDictionary<Endpoint, Endpoint?> Dijkstra(IEnumerable<Endpoint> nodes, Endpoint origin, Endpoint destination)
+        {
+            // Dijkstra's shortest path algorithm -- based on https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+            var unvisited = new List<Endpoint>();
+            var dist = new Dictionary<Endpoint, int>();
+            var prev = new Dictionary<Endpoint, Endpoint?>();
+
+            Initialize();
+
+            while (unvisited.Any())
+            {
+                var nearest = unvisited.OrderBy(it => dist[it]).First();
+                unvisited.Remove(nearest);
+
+                if (nearest == destination)
+                    return prev;
+
+                UpdateCosts(nearest);
+            }
+
+            return new Dictionary<Endpoint, Endpoint?>();
+
+            //
+
+            void Initialize()
+            {
+                foreach (var v in nodes)
+                {
+                    dist[v] = int.MaxValue;
+                    prev[v] = null;
+                    unvisited.Add(v);
+                }
+
+                dist[origin] = 0;
+            }
+
+            void UpdateCosts(Endpoint e)
+            {
+                // all links containing e where the other endpoint is unvisited
+                var unvisitedLinks = links.Where(it => it.Contains(e) && unvisited.Contains(it.GetOther(e)));
+                foreach (var link in unvisitedLinks)
+                    UpdateCost(link, e);
+            }
+
+            void UpdateCost(Link link, Endpoint e)
+            {
+                var v = link.GetOther(e);
+
+                var alt = dist[e] + link.Cost;
+                // ReSharper disable once InvertIf
+                if (alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = e;
+                }
+            }
+        }
+
         private static IEnumerable<Endpoint> BuildPath(IReadOnlyDictionary<Endpoint, Endpoint?> prev, Endpoint origin, Endpoint destination)
         {
             var u = destination;
@@ -82,6 +106,7 @@ namespace TransportTycoon.Library.Services
 
             var result = new List<Endpoint>();
 
+            // ReSharper disable once SuggestVarOrType_SimpleTypes
             Endpoint? uu = u;
             while (uu != null)
             {
@@ -100,12 +125,6 @@ namespace TransportTycoon.Library.Services
                 yield return links.First(it => it == new Link(origin, destination, 0));
                 origin = destination;
             }
-        }
-
-        // return all links containing u where the other endpoint is in q
-        private IEnumerable<Link> GetLinks(Endpoint u, IReadOnlyList<Endpoint> q)
-        {
-            return links.Where(it => it.Contains(u) && q.Contains(it.GetOther(u))).ToArray();
         }
     }
 }
