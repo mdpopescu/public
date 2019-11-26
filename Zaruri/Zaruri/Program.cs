e6ca3d3d978ad4ed67ba95bdc2c7654a84323ab3
Marcel Popescu <mdpopescu@gmail.com>
2019-11-26 20:21:50 +0200

    Refactoring

--- Zaruri/Zaruri/Program.cs
+++ Zaruri/Zaruri/Program.cs
@@ -5,8 +5,6 @@ using System.Linq;
 
 namespace Zaruri
 {
-    [SuppressMessage("ReSharper", "MethodTooLong")]
-    [SuppressMessage("ReSharper", "TooManyDeclarations")]
     internal class Program
     {
         private static void Main()
@@ -14,19 +12,14 @@ namespace Zaruri
             var amount = 100;
             while (amount > 0)
             {
-                amount -= 1;
-                Console.WriteLine($"1$ bet; current amount: {amount}$");
+                MakeBet(ref amount);
 
-                var roll1 = GenerateDice().ToArray();
-                ShowRoll("Initial roll", roll1);
-
-                var indices = ReadIndices();
-                var roll2 = roll1.Select((value, i) => indices.Contains(i + 1) ? value : GenerateDie()).ToArray();
-                ShowRoll("Final roll", roll2);
+                var roll1 = InitialRoll();
+                var roll2 = FinalRoll(roll1);
 
                 var hand = GetHand(roll2);
-                amount += hand.Score;
-                Console.WriteLine($"{hand.Name}: {hand.Score}$ -- amount {amount}$");
+                ComputeResult(hand, ref amount);
+
                 Console.WriteLine();
             }
         }
@@ -37,6 +30,32 @@ namespace Zaruri
 
         private static IEnumerable<int> GenerateDice() => Enumerable.Range(1, Constants.DICE_COUNT).Select(_ => GenerateDie()).ToArray();
         private static int GenerateDie() => RND.Next(1, Constants.DIE_MAX + 1);
+
+        private static void MakeBet(ref int amount)
+        {
+            amount -= 1;
+            Console.WriteLine($"1$ bet; current amount: {amount}$");
+        }
+
+        private static IEnumerable<int> InitialRoll()
+        {
+            var roll1 = GenerateDice().ToArray();
+            ShowRoll("Initial roll", roll1);
+
+            return roll1;
+        }
+
+        private static int[] FinalRoll(IEnumerable<int> roll1)
+        {
+            var indices = ReadIndices();
+
+            var roll2 = roll1.Select((value, i) => indices.Contains(i + 1) ? value : GenerateDie()).ToArray();
+            ShowRoll("Final roll", roll2);
+
+            return roll2;
+        }
+
+        private static void ShowRoll(string prefix, IEnumerable<int> roll) => Console.WriteLine(prefix + $": {string.Join(" ", roll)}");
 
         private static Indices ReadIndices()
         {
@@ -60,20 +79,24 @@ namespace Zaruri
             return Indices.Create(values);
         }
 
-        private static void ShowRoll(string prefix, IEnumerable<int> roll) => Console.WriteLine(prefix + $": {string.Join(" ", roll)}");
+        private static void ComputeResult(Hand hand, ref int amount)
+        {
+            amount += hand.Score;
+            Console.WriteLine($"{hand.Name}: {hand.Score}$ -- amount {amount}$");
+        }
 
         [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
         private static Hand GetHand(int[] roll)
         {
             roll = roll.OrderBy(it => it).ToArray();
 
+            var groups = roll.GroupBy(it => it).ToArray();
+            var groupCounts = groups.Select(it => it.Count()).OrderByDescending(it => it).ToArray();
+
             if (roll.SequenceEqual(new[] { 2, 3, 4, 5, 6 }))
                 return new HighFlush();
             if (roll.SequenceEqual(new[] { 1, 2, 3, 4, 5 }))
                 return new LowFlush();
-
-            var groups = roll.GroupBy(it => it).ToArray();
-            var groupCounts = groups.Select(it => it.Count()).OrderByDescending(it => it).ToArray();
 
             if (groupCounts.SequenceEqual(new[] { 5 }))
                 return new FiveOfAKind();
