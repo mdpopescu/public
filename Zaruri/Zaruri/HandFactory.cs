a76e0b959e86b57269c172f4f95d697f4df06aaa
Marcel Popescu <mdpopescu@gmail.com>
2019-11-26 21:14:25 +0200

    Refactoring

---
+++ Zaruri/Zaruri/HandFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Zaruri
+{
+    public class HandFactory
+    {
+        public Hand Create(int[] roll)
+        {
+            roll = roll.OrderBy(it => it).ToArray();
+
+            var groups = roll.GroupBy(it => it).ToArray();
+            var groupCounts = groups.Select(it => it.Count()).OrderByDescending(it => it).ToArray();
+
+            if (roll.SequenceEqual(new[] { 2, 3, 4, 5, 6 }))
+                return new HighFlush();
+            if (roll.SequenceEqual(new[] { 1, 2, 3, 4, 5 }))
+                return new LowFlush();
+
+            if (groupCounts.SequenceEqual(new[] { 5 }))
+                return new FiveOfAKind();
+            if (groupCounts.SequenceEqual(new[] { 4, 1 }))
+                return new FourOfAKind();
+            if (groupCounts.SequenceEqual(new[] { 3, 2 }))
+                return new FullHouse();
+            if (groupCounts.SequenceEqual(new[] { 3, 1, 1 }))
+                return new ThreeOfAKind();
+            if (groupCounts.SequenceEqual(new[] { 2, 2, 1 }))
+                return new TwoPairs();
+            if (groupCounts.SequenceEqual(new[] { 2, 1, 1, 1 }))
+                return new OnePair();
+
+            return new Nothing();
+        }
+    }
+}
--- Zaruri/Zaruri/Program.cs
+++ Zaruri/Zaruri/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Zaruri
@@ -17,7 +16,7 @@ namespace Zaruri
                 var roll1 = InitialRoll();
                 var roll2 = FinalRoll(roll1);
 
-                var hand = GetHand(roll2);
+                var hand = FACTORY.Create(roll2);
                 ComputeResult(hand, ref amount);
 
                 Console.WriteLine();
@@ -27,6 +26,7 @@ namespace Zaruri
         //
 
         private static readonly Random RND = new Random();
+        private static readonly HandFactory FACTORY = new HandFactory();
 
         private static IEnumerable<int> GenerateDice() => Enumerable.Range(1, Constants.DICE_COUNT).Select(_ => GenerateDie()).ToArray();
         private static int GenerateDie() => RND.Next(1, Constants.DIE_MAX + 1);
@@ -83,35 +83,6 @@ namespace Zaruri
         {
             amount += hand.Score;
             Console.WriteLine($"{hand.Name}: {hand.Score}$ -- amount {amount}$");
-        }
-
-        [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
-        private static Hand GetHand(int[] roll)
-        {
-            roll = roll.OrderBy(it => it).ToArray();
-
-            var groups = roll.GroupBy(it => it).ToArray();
-            var groupCounts = groups.Select(it => it.Count()).OrderByDescending(it => it).ToArray();
-
-            if (roll.SequenceEqual(new[] { 2, 3, 4, 5, 6 }))
-                return new HighFlush();
-            if (roll.SequenceEqual(new[] { 1, 2, 3, 4, 5 }))
-                return new LowFlush();
-
-            if (groupCounts.SequenceEqual(new[] { 5 }))
-                return new FiveOfAKind();
-            if (groupCounts.SequenceEqual(new[] { 4, 1 }))
-                return new FourOfAKind();
-            if (groupCounts.SequenceEqual(new[] { 3, 2 }))
-                return new FullHouse();
-            if (groupCounts.SequenceEqual(new[] { 3, 1, 1 }))
-                return new ThreeOfAKind();
-            if (groupCounts.SequenceEqual(new[] { 2, 2, 1 }))
-                return new TwoPairs();
-            if (groupCounts.SequenceEqual(new[] { 2, 1, 1, 1 }))
-                return new OnePair();
-
-            return new Nothing();
         }
     }
 }
