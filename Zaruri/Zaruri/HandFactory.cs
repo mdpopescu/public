fa749739e69c9b81da8b1f0ebf8c87ffa8000dbe
Marcel Popescu <mdpopescu@gmail.com>
2019-11-26 21:25:18 +0200

    Refactoring

--- Zaruri/Zaruri/HandFactory.cs
+++ Zaruri/Zaruri/HandFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Zaruri
 {
@@ -11,25 +13,40 @@ namespace Zaruri
             var groups = roll.GroupBy(it => it).ToArray();
             var groupCounts = groups.Select(it => it.Count()).OrderByDescending(it => it).ToArray();
 
-            if (roll.SequenceEqual(new[] { 2, 3, 4, 5, 6 }))
-                return new HighFlush();
-            if (roll.SequenceEqual(new[] { 1, 2, 3, 4, 5 }))
-                return new LowFlush();
+            return CreateFromRoll(roll)
+                ?? CreateFromGroupCounts(groupCounts)
+                ?? new Nothing();
+        }
 
-            if (groupCounts.SequenceEqual(new[] { 5 }))
-                return new FiveOfAKind();
-            if (groupCounts.SequenceEqual(new[] { 4, 1 }))
-                return new FourOfAKind();
-            if (groupCounts.SequenceEqual(new[] { 3, 2 }))
-                return new FullHouse();
-            if (groupCounts.SequenceEqual(new[] { 3, 1, 1 }))
-                return new ThreeOfAKind();
-            if (groupCounts.SequenceEqual(new[] { 2, 2, 1 }))
-                return new TwoPairs();
-            if (groupCounts.SequenceEqual(new[] { 2, 1, 1, 1 }))
-                return new OnePair();
+        //
 
-            return new Nothing();
+        private static readonly List<(int[], Type)> ROLL_MAP = new List<(int[], Type)>
+        {
+            (new[] { 2, 3, 4, 5, 6 }, typeof(HighFlush)),
+            (new[] { 1, 2, 3, 4, 5 }, typeof(LowFlush)),
+        };
+
+        private static readonly List<(int[], Type)> GROUP_COUNTS_MAP = new List<(int[], Type)>
+        {
+            (new[] { 5 }, typeof(FiveOfAKind)),
+            (new[] { 4, 1 }, typeof(FourOfAKind)),
+            (new[] { 3, 2 }, typeof(FullHouse)),
+            (new[] { 3, 1, 1 }, typeof(ThreeOfAKind)),
+            (new[] { 2, 2, 1 }, typeof(TwoPairs)),
+            (new[] { 2, 1, 1, 1 }, typeof(OnePair)),
+        };
+
+        private static Hand CreateFromRoll(int[] roll) => CreateFromMap(ROLL_MAP, roll);
+
+        private static Hand CreateFromGroupCounts(int[] groupCounts) => CreateFromMap(GROUP_COUNTS_MAP, groupCounts);
+
+        private static Hand CreateFromMap(IEnumerable<(int[], Type)> map, int[] sequence)
+        {
+            return map
+                .Where(it => it.Item1.SequenceEqual(sequence))
+                .Select(it => it.Item2)
+                .Select(type => (Hand) Activator.CreateInstance(type))
+                .FirstOrDefault();
         }
     }
 }
