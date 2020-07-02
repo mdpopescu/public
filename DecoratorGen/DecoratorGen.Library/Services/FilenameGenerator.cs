7bbd58436e2646f528f0c6ffc53b75f9f95a220f
Marcel Popescu <mdpopescu@gmail.com>
2020-07-02 20:24:38 +0300

    Implemented the FilenameGenerator

--- DecoratorGen/DecoratorGen.Library/Services/FilenameGenerator.cs
+++ DecoratorGen/DecoratorGen.Library/Services/FilenameGenerator.cs
@@ -1,9 +1,20 @@
-﻿using DecoratorGen.Library.Contracts;
+﻿using System.IO;
+using DecoratorGen.Library.Contracts;
 
 namespace DecoratorGen.Library.Services
 {
     public class FilenameGenerator : IFilenameGenerator
     {
-        public string Generate(string filename) => "";
+        public string Generate(string filename)
+        {
+            var fileOnly = Path.GetFileNameWithoutExtension(filename);
+            var firstTwoLetters = fileOnly.Substring(0, 2);
+            var newFile = firstTwoLetters.Length == 2 && firstTwoLetters[0] == 'I' && char.IsUpper(firstTwoLetters[1])
+                ? fileOnly.Substring(1)
+                : fileOnly;
+            var extension = Path.GetExtension(filename);
+
+            return Path.Combine(Path.GetDirectoryName(filename) + "", $"{newFile}Decorator{extension}");
+        }
     }
 }
---
+++ DecoratorGen/DecoratorGen.Tests/Services/FilenameGeneratorTests.cs
@@ -0,0 +1,51 @@
+﻿using DecoratorGen.Library.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DecoratorGen.Tests.Services
+{
+    [TestClass]
+    public class FilenameGeneratorTests
+    {
+        private FilenameGenerator sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new FilenameGenerator();
+        }
+
+        [TestClass]
+        public class Generate : FilenameGeneratorTests
+        {
+            [TestMethod]
+            public void GeneratesTheCorrectNameForFilesStartingWithTheLetterIFollowedByAnotherUppercaseLetter()
+            {
+                const string FILENAME = @"c:\temp\ISomething.cs";
+
+                var result = sut.Generate(FILENAME);
+
+                Assert.AreEqual(@"c:\temp\SomethingDecorator.cs", result);
+            }
+
+            [TestMethod]
+            public void GeneratesTheCorrectNameForFilesNotStartingWithTheLetterI()
+            {
+                const string FILENAME = @"c:\temp\Something.cs";
+
+                var result = sut.Generate(FILENAME);
+
+                Assert.AreEqual(@"c:\temp\SomethingDecorator.cs", result);
+            }
+
+            [TestMethod]
+            public void GeneratesTheCorrectNameForFilesStartingWithTheLetterIFollowedByALowercaseLetter()
+            {
+                const string FILENAME = @"c:\temp\Interesting.cs";
+
+                var result = sut.Generate(FILENAME);
+
+                Assert.AreEqual(@"c:\temp\InterestingDecorator.cs", result);
+            }
+        }
+    }
+}
