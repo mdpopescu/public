74742d3c7a2d2de155209560dff9620f8fe4e1c6
Marcel Popescu <mdpopescu@gmail.com>
2020-07-02 20:26:03 +0300

    Implemented the FileSystem class

---
+++ DecoratorGen/DecoratorGen.Library/Services/FileSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using DecoratorGen.Library.Contracts;
+
+namespace DecoratorGen.Library.Services
+{
+    public class FileSystem : IFileSystem
+    {
+        public string ReadText(string filename) =>
+            File.ReadAllText(filename);
+
+        public void WriteText(string filename, string text) =>
+            File.WriteAllText(filename, text);
+    }
+}
