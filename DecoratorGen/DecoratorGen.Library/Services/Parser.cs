0e0a031ea3cb836c218147ccf2c7a32b93c3e23e
Marcel Popescu <mdpopescu@gmail.com>
2020-07-02 21:40:33 +0300

    Implemented the Parser.ExtractInterface method

--- DecoratorGen/DecoratorGen.Library/Services/Parser.cs
+++ DecoratorGen/DecoratorGen.Library/Services/Parser.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using DecoratorGen.Library.Contracts;
 using DecoratorGen.Library.Models;
 
@@ -7,7 +9,47 @@ namespace DecoratorGen.Library.Services
 {
     public class Parser : IParser
     {
-        public InterfaceData ExtractInterface(string code) => new InterfaceData();
+        public InterfaceData ExtractInterface(string code)
+        {
+            var lines = code
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                .SelectMany(line => line.Split('\r', '\n'))
+                .ToList();
+
+            while (lines.Count > 0 && !RE1.IsMatch(lines[0]))
+                lines.RemoveAt(0);
+
+            // count the { and } characters until they match
+            int? balance = null;
+            for (var i = 0; i < lines.Count; i++)
+            {
+                var openCount = lines[i].Count(it => it == '{');
+                var closedCount = lines[i].Count(it => it == '}');
+
+                if (openCount > 0)
+                    balance = balance.GetValueOrDefault() + openCount;
+                if (closedCount > 0)
+                    balance = balance.GetValueOrDefault() - closedCount;
+
+                // if there's no { on the "interface ISomething" line, that doesn't mean they're balanced
+                // we need to encounter at least one { or } before we can decide that
+                if (balance.HasValue && balance.Value <= 0)
+                    lines = lines.Take(i + 1).ToList();
+            }
+
+            var interfaceCode = string.Join(Environment.NewLine, lines);
+
+            var match = RE2.Match(interfaceCode);
+            var name = match.Groups[1].Value;
+
+            return new InterfaceData { Name = name, Code = interfaceCode };
+        }
+
         public IEnumerable<Member> ExtractMembers(string code) => Enumerable.Empty<Member>();
+
+        //
+
+        private static readonly Regex RE1 = new Regex("interface\\s+\\w+", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex RE2 = new Regex("interface\\s+(\\w+)\\s*\\{", RegexOptions.IgnoreCase | RegexOptions.Compiled);
     }
 }
---
+++ DecoratorGen/DecoratorGen.Tests/Services/ParserTests.cs
@@ -0,0 +1,52 @@
+﻿using DecoratorGen.Library.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DecoratorGen.Tests.Services
+{
+    [TestClass]
+    public class ParserTests
+    {
+        private Parser sut;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            sut = new Parser();
+        }
+
+        [TestClass]
+        public class ExtractInterface : ParserTests
+        {
+            [TestMethod]
+            public void ExtractsTheFirstInterfaceFromTheGivenCodeFragment()
+            {
+                const string FRAGMENT = @"class A
+{
+    public int B { get; set; }
+}
+
+public interface IX
+{
+    public string C { get; set; }
+}
+
+internal class D
+{
+}
+
+public interface IY
+{}
+";
+                var result = sut.ExtractInterface(FRAGMENT);
+
+                Assert.AreEqual("IX", result.Name);
+                Assert.AreEqual(
+                    @"public interface IX
+{
+    public string C { get; set; }
+}",
+                    result.Code);
+            }
+        }
+    }
+}
