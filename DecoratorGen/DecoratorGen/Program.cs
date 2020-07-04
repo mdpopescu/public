283ce989224ed03cfdf56012b2dfba781d17ab90
Marcel Popescu <mdpopescu@gmail.com>
2020-07-04 17:17:03 +0300

    Added a console app and a clipboard output

---
+++ DecoratorGen/DecoratorGen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using DecoratorGen.Library.Services;
+using DecoratorGen.Services;
+
+namespace DecoratorGen
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: DecoratorGen <path-to-interface.cs>");
+                Console.WriteLine();
+                Console.WriteLine("       This will parse the interface, generate a decorator class for it and place the resulting code on the clipboard.");
+
+                return;
+            }
+
+            var filename = args[0];
+
+            var path = Path.GetDirectoryName(filename);
+            var file = Path.GetFileName(filename);
+
+            var fs = new FileSystem(path);
+            var output = new ClipboardOutput();
+            var codeGenerator = new CodeGenerator(
+                new Parser(),
+                new MemberGenerator(),
+                new ClassNameGenerator()
+            );
+            var app = new App(fs, codeGenerator, output);
+
+            app.GenerateDecorator(file);
+        }
+    }
+}
---
+++ DecoratorGen/DecoratorGen/Services/ClipboardOutput.cs
@@ -0,0 +1,14 @@
+﻿using DecoratorGen.Library.Contracts;
+using TextCopy;
+
+namespace DecoratorGen.Services
+{
+    public class ClipboardOutput : IOutput
+    {
+        public void WriteCode(string code) => CLIPBOARD.SetText(code);
+
+        //
+
+        private static readonly Clipboard CLIPBOARD = new Clipboard();
+    }
+}
