4a6ac8a40008ec36547715605a718a3de8f15120
Marcel Popescu <mdpopescu@gmail.com>
2021-10-17 22:06:52 +0300

    Bad attempt

--- NoIfsChallenges/Challenge2/Challenge2.Rx/MainForm.cs
+++ NoIfsChallenges/Challenge2/Challenge2.Rx/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Reactive;
 using System.Reactive.Disposables;
@@ -21,10 +22,16 @@ namespace Challenge2.Rx
         {
             base.OnLoad(e);
 
+            // external events
+
             var startStopClicked = btnStartStop.GetClicks().CreateLog("startStopClicked").Share();
             var resetClicked = btnReset.GetClicks().CreateLog("resetClicked").Share();
             var holdClicked = btnHold.GetClicks().CreateLog("holdClicked").Share();
+            IObservable<TimeSpan> CreateTimer() => Observable.Interval(SECOND).StartWith(0).Select(value => TimeSpan.FromSeconds(value)).CreateLog("timerValue").Share();
 
+            // derived events
+
+            var startClicked = startStopClicked.Toggle(false).Where(it => it).AsUnit().CreateLog("startClicked");
             var stopClicked = startStopClicked.Buffer(2).AsUnit().CreateLog("stopClicked");
 
             var ssEnabledFalse = stopClicked.AsConst(false).CreateLog("ssEnabledFalse");
@@ -35,22 +42,26 @@ namespace Challenge2.Rx
             var resetEnabledFalse = resetClicked.AsConst(false).CreateLog("resetEnabledFalse");
             var resetEnabled = resetEnabledTrue.Merge(resetEnabledFalse).StartWith(false).CreateLog("resetEnabled");
 
-            var holdEnabled = startStopClicked.Toggle(false).CreateLog("holdEnabled");
+            var holdEnabled = startClicked.CreateLog("holdEnabled");
 
-            var clearHold = resetClicked.AsUnit().StartWith(Unit.Default).CreateLog("clearHold");
-            var shouldDisplay = clearHold.SwitchMap(_ => holdClicked.Toggle(true)).Share().CreateLog("shouldDisplay");
+            var unfreeze = holdClicked.Buffer(2).AsUnit().CreateLog("unfreeze");
+            var displayEnabled = startClicked.Merge(unfreeze).CreateLog("displayEnabled");
 
-            var timer = startStopClicked.Toggle(false).WhenTrue(() => StartTimer().CreateLog("startTimer").Share()).CreateLog("timer");
+            var timer = startClicked.SwitchMap(_ => CreateTimer().TakeUntil(stopClicked).CreateLog("CreateTimer")).CreateLog("timer");
+            var display = timer.Select(it => it.ToString()).CreateLog("display");
 
-            var timerRunning = timer.CombineLatest(shouldDisplay).Where(it => it.Item2).Select(it => it.Item1).CreateLog("timerRunning");
-            var timerReset = resetClicked.Select(_ => TimeSpan.Zero).CreateLog("timerReset");
-            var displayActive = timerRunning.Merge(timerReset).Select(value => value.ToString()).CreateLog("displayActive");
+            //var clearHold = resetClicked.StartWith(Unit.Default).CreateLog("clearHold");
+            //var shouldDisplay = clearHold.SwitchMap(_ => holdClicked.Toggle(true)).Share().CreateLog("shouldDisplay");
+
+            //var timerRunning = timer.CombineLatest(shouldDisplay).Where(it => it.Item2).Select(it => it.Item1).CreateLog("timerRunning");
+            //var timerReset = resetClicked.Select(_ => TimeSpan.Zero).CreateLog("timerReset");
+            //var displayActive = timerRunning.Merge(timerReset).Select(value => value.ToString()).CreateLog("displayActive");
 
             var s1 = btnStartStop.HandleChanges(ssEnabled, InternalSetEnabled);
             var s2 = btnReset.HandleChanges(resetEnabled, InternalSetEnabled);
             var s3 = btnHold.HandleChanges(holdEnabled, InternalSetEnabled);
 
-            var s4 = lblClock.HandleChanges(displayActive, InternalSetText);
+            var s4 = lblClock.HandleChanges(display, InternalSetText);
 
             subscription = new CompositeDisposable(s1, s2, s3, s4);
         }
