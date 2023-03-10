e5c19c62bbd06d077cfc3bb8da6174ea91030afe
Marcel Popescu <mdpopescu@gmail.com>
2023-03-10 21:53:32 +0200

    Lists all the controls in a Notepad application (assuming at least one instance of Notepad is running)

---
+++ CustomDialogs/CustomDialogs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace CustomDialogs;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // we're going to look for a Notepad window
+        var notepad = Process
+            .GetProcessesByName("Notepad")
+            .Select(it => it.MainWindowHandle)
+            .Select(AutomationElement.FromHandle)
+            .FirstOrDefault();
+        if (notepad == null)
+        {
+            Console.WriteLine("Notepad not found.");
+            return;
+        }
+
+        ListElements(notepad);
+    }
+
+    private static void ListElements(AutomationElement parent, int indent = 0)
+    {
+        var walker = TreeWalker.ControlViewWalker;
+
+        var control = walker.GetFirstChild(parent);
+        while (control != null)
+        {
+            Console.WriteLine(new string(' ', indent * 2) + control.Current.LocalizedControlType + ": " + control.Current.Name);
+            ListElements(control, indent + 1);
+
+            control = walker.GetNextSibling(control);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -31,6 +31,8 @@ Public repository - CC-by-SA 3.0
 
 * CustomCrypto: a custom encryption / decryption algorithm (NOT recommended, I am not a professional cryptographer!)
 
+* CustomDialogs: replace the File Open / File Save dialogs in a different application with custom versions
+
 * DecoratorGen: an utility to generate decorators around an interface (a class implementing that interface by delegating to another interface of the same type)
 
 * DesktopClock: a simple desktop wallpaper clock - trying to learn WPF
