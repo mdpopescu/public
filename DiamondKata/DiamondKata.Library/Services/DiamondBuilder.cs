57132e09d2f5dffaa616e7218c163c382abe20f4
Marcel Popescu <mdpopescu@gmail.com>
2024-05-03 14:39:47 +0300

    The second test passes

--- DiamondKata/DiamondKata.Library/Services/DiamondBuilder.cs
+++ DiamondKata/DiamondKata.Library/Services/DiamondBuilder.cs
@@ -4,6 +4,17 @@ public class DiamondBuilder
 {
     public void Build(string[] args, TextWriter writer)
     {
-        //
+        if (args.Length != 1)
+            return;
+
+        var arg = args[0].ToUpperInvariant();
+        if (arg.Length != 1)
+            return;
+
+        var ch = arg[0];
+        if (ch is < 'A' or > 'Z')
+            return;
+
+        writer.WriteLine(ch.ToString());
     }
 }
