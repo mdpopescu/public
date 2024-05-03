b8dc492b266083583a52c76fc2d27bae0ed27145
Marcel Popescu <mdpopescu@gmail.com>
2024-05-03 14:34:52 +0300

    The first test fails because of the NotImplemented exception

--- DiamondKata/DiamondKata.Tests/Services/DiamondBuilderTests.cs
+++ DiamondKata/DiamondKata.Tests/Services/DiamondBuilderTests.cs
@@ -1,15 +1,35 @@
 ﻿using DiamondKata.Library.Services;
+using NSubstitute;
 
 namespace DiamondKata.Tests.Services;
 
 [TestClass]
 public class DiamondBuilderTests
 {
-    private DiamondBuilder sut;
+    private readonly DiamondBuilder sut;
 
     // ReSharper disable once ConvertConstructorToMemberInitializers
     public DiamondBuilderTests()
     {
         sut = new DiamondBuilder();
+    }
+
+    [TestClass]
+    public class Build : DiamondBuilderTests
+    {
+        private readonly TextWriter writer = Substitute.For<TextWriter>();
+
+        [TestMethod("Out of range or multiple characters do not write anything to the output")]
+        [DataRow("0")]
+        [DataRow("!")]
+        [DataRow("~")]
+        [DataRow("\\u1F600")]
+        [DataRow("ABC")]
+        public void Test1(string arg)
+        {
+            sut.Build([arg], writer);
+
+            writer.DidNotReceive().WriteLine(Arg.Any<string>());
+        }
     }
 }
